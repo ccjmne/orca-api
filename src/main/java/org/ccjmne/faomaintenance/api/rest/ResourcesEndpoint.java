b9d32e412b0510a29d514e460f20c6dbfb1c06b9
Eric NICOLAS <ccjmne@gmail.com>
2015-02-03 17:21:08 +0100

    Now returning Records instead of Maps in ResourceEndpoint. Enhanced the JSON serialiser to support these objects.

--- src/main/java/org/ccjmne/faomaintenance/api/rest/ResourcesEndpoint.java
+++ src/main/java/org/ccjmne/faomaintenance/api/rest/ResourcesEndpoint.java
@@ -3,7 +3,6 @@ package org.ccjmne.faomaintenance.api.rest;
 import java.text.ParseException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -18,9 +17,8 @@ import org.ccjmne.faomaintenance.api.utils.SQLDateFormat;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.Record;
+import org.jooq.Result;
 import org.jooq.SelectQuery;
-import org.jooq.exception.DataAccessException;
-import org.jooq.exception.InvalidResultException;
 
 @Path("resources")
 public class ResourcesEndpoint {
@@ -36,17 +34,16 @@ public class ResourcesEndpoint {
 
 	@GET
 	@Path("employees")
-	public List<Map<String, Object>> listEmployees(@QueryParam("site") final String aurore, @QueryParam("date") final String dateStr)
-			throws InvalidResultException, DataAccessException, ParseException {
+	public List<Record> listEmployees(@QueryParam("site") final String aurore, @QueryParam("date") final String dateStr) throws ParseException {
 		try {
 			final SelectQuery<Record> query = this.ctx.selectQuery(Tables.EMPLOYEES.join(Tables.SITES_EMPLOYEES)
-			                                                       .on(Tables.SITES_EMPLOYEES.SIEM_EMPL_FK.eq(Tables.EMPLOYEES.EMPL_PK)));
+					.on(Tables.SITES_EMPLOYEES.SIEM_EMPL_FK.eq(Tables.EMPLOYEES.EMPL_PK)));
 			query.addConditions(Tables.SITES_EMPLOYEES.SIEM_UPDT_FK.eq(getUpdateFor(dateStr)));
 			if (aurore != null) {
 				query.addConditions(Tables.SITES_EMPLOYEES.SIEM_SITE_FK.eq(aurore));
 			}
 
-			return query.fetchMaps();
+			return query.fetch();
 		} catch (final NoDataException e) {
 			return Collections.EMPTY_LIST;
 		}
@@ -54,25 +51,25 @@ public class ResourcesEndpoint {
 
 	@GET
 	@Path("employees/{registrationNumber}")
-	public Map<String, Object> lookupEmployee(@PathParam("registrationNumber") final String registrationNumber) {
-		return this.ctx.select().from(Tables.EMPLOYEES).where(Tables.EMPLOYEES.EMPL_PK.equal(registrationNumber)).fetchOne().intoMap();
+	public Record lookupEmployee(@PathParam("registrationNumber") final String registrationNumber) {
+		return this.ctx.select().from(Tables.EMPLOYEES).where(Tables.EMPLOYEES.EMPL_PK.equal(registrationNumber)).fetchOne();
 	}
 
 	@GET
 	@Path("sites")
-	public List<Map<String, Object>> listSites(@QueryParam("department") final Integer department) {
+	public List<SitesRecord> listSites(@QueryParam("department") final Integer department) {
 		final SelectQuery<SitesRecord> query = this.ctx.selectQuery(Tables.SITES);
 		if (department != null) {
 			query.addConditions(Tables.SITES.SITE_DEPT_FK.eq(department));
 		}
 
-		return query.fetchMaps();
+		return query.fetch();
 	}
 
 	@GET
 	@Path("sites/{aurore}")
-	public Map<String, Object> lookupSite(@PathParam("aurore") final String aurore) {
-		return this.ctx.select().from(Tables.SITES).where(Tables.SITES.SITE_PK.equal(aurore)).fetchOne().intoMap();
+	public Record lookupSite(@PathParam("aurore") final String aurore) {
+		return this.ctx.select().from(Tables.SITES).where(Tables.SITES.SITE_PK.equal(aurore)).fetchOne();
 	}
 
 	private Integer getLatestUpdateWhere(final Condition... conditions) {
@@ -94,45 +91,63 @@ public class ResourcesEndpoint {
 
 	@GET
 	@Path("trainings")
-	public List<Map<String, Object>> listTrainings(@QueryParam("employee") final String registrationNumber, @QueryParam("type") final List<Integer> types) {
+	public Result<Record> listTrainings(
+	                                    @QueryParam("employee") final String registrationNumber,
+	                                    @QueryParam("type") final List<Integer> types,
+	                                    @QueryParam("date") final String dateStr,
+	                                    @QueryParam("from") final String fromStr,
+	                                    @QueryParam("to") final String toStr) throws ParseException {
 		final SelectQuery<Record> query = this.ctx.selectQuery();
 		query.addFrom(Tables.TRAININGS);
 		if (registrationNumber != null) {
 			query.addJoin(
-							Tables.TRAININGS_EMPLOYEES,
-							Tables.TRAININGS_EMPLOYEES.TREM_TRNG_FK.eq(Tables.TRAININGS.TRNG_PK).and(
-																										Tables.TRAININGS_EMPLOYEES.TREM_EMPL_FK
-																												.eq(registrationNumber)));
+			              Tables.TRAININGS_EMPLOYEES,
+			              Tables.TRAININGS_EMPLOYEES.TREM_TRNG_FK.eq(Tables.TRAININGS.TRNG_PK).and(
+			                                                                                       Tables.TRAININGS_EMPLOYEES.TREM_EMPL_FK
+			                                                                                       .eq(registrationNumber)));
 		}
 
 		if (!types.isEmpty()) {
 			query.addJoin(Tables.TRAININGTYPES, Tables.TRAININGS.TRNG_TRTY_FK.eq(Tables.TRAININGTYPES.TRTY_PK).and(Tables.TRAININGTYPES.TRTY_PK.in(types)));
 		}
 
-		return query.fetchMaps();
+		if (dateStr != null) {
+			query.addConditions(Tables.TRAININGS.TRNG_DATE.eq(this.dateFormat.parseSql(dateStr)));
+		}
+
+		if (fromStr != null) {
+			query.addConditions(Tables.TRAININGS.TRNG_DATE.ge(this.dateFormat.parseSql(fromStr)));
+		}
+
+		if (toStr != null) {
+			query.addConditions(Tables.TRAININGS.TRNG_DATE.le(this.dateFormat.parseSql(toStr)));
+		}
+
+		query.addOrderBy(Tables.TRAININGS.TRNG_DATE);
+		return query.fetch();
 	}
 
 	@GET
 	@Path("trainings/{id}")
-	public Map<String, Object> lookupTraining(@PathParam("id") final Integer id) {
-		return this.ctx.select().from(Tables.TRAININGS).where(Tables.TRAININGS.TRNG_PK.equal(id)).fetchOne().intoMap();
+	public Record lookupTraining(@PathParam("id") final Integer id) {
+		return this.ctx.select().from(Tables.TRAININGS).where(Tables.TRAININGS.TRNG_PK.equal(id)).fetchOne();
 	}
 
 	@GET
 	@Path("departments")
-	public List<Map<String, Object>> listDepartments() {
-		return this.ctx.select().from(Tables.DEPARTMENTS).fetchMaps();
+	public Result<Record> listDepartments() {
+		return this.ctx.select().from(Tables.DEPARTMENTS).fetch();
 	}
 
 	@GET
 	@Path("training_types")
-	public List<Map<String, Object>> listTrainingTypes() {
-		return this.ctx.select().from(Tables.TRAININGTYPES).fetchMaps();
+	public Result<Record> listTrainingTypes() {
+		return this.ctx.select().from(Tables.TRAININGTYPES).fetch();
 	}
 
 	@GET
 	@Path("certificates")
-	public List<Map<String, Object>> listCertificates() {
-		return this.ctx.select().from(Tables.CERTIFICATES).fetchMaps();
+	public Result<Record> listCertificates() {
+		return this.ctx.select().from(Tables.CERTIFICATES).fetch();
 	}
 }
