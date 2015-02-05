1161be0aa7e9b939c7837341b1590878d6aa0db3
Eric NICOLAS <ccjmne@gmail.com>
2015-02-05 01:33:56 +0100

    Now properly handling DB Connections, with a DB Connections Pool (HikariCP). Also switched from logback to slf4j for Logging since it's already pulled in by several other dependencies.

--- src/main/java/org/ccjmne/faomaintenance/api/utils/PostgresDSLContext.java
+++ src/main/java/org/ccjmne/faomaintenance/api/utils/PostgresDSLContext.java
@@ -1,13 +1,13 @@
 package org.ccjmne.faomaintenance.api.utils;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import org.jooq.SQLDialect;
 import org.jooq.conf.Settings;
 import org.jooq.impl.DefaultDSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @SuppressWarnings("serial")
 public class PostgresDSLContext extends DefaultDSLContext {
@@ -20,20 +20,33 @@ public class PostgresDSLContext extends DefaultDSLContext {
 	private static final String DB_USER = System.getProperty("db_user", "postgres");
 	private static final String DB_PASS = System.getProperty("db_pass", "asdf123");
 	private static final String DB_URL = String.format(
-													   "jdbc:postgresql://%s:%s/%s",
+														"jdbc:postgresql://%s:%s/%s",
 														System.getProperty("db_host", "localhost"),
 														System.getProperty("db_port", "5432"),
 														System.getProperty("db_name", "postgres"));
+
+	private static HikariDataSource DATA_SOURCE;
 	static {
 		try {
 			Class.forName(DB_DRIVER);
 		} catch (final ClassNotFoundException e) {
 			LOGGER.error("Could not load DB driver: {}", DB_DRIVER);
 		}
+
+		final HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(DB_URL);
+		config.setUsername(DB_USER);
+		config.setPassword(DB_PASS);
+		config.setMaximumPoolSize(18);
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		config.addDataSourceProperty("useServerPrepStmts", "true");
+		DATA_SOURCE = new HikariDataSource(config);
 	}
 
-	public PostgresDSLContext() throws SQLException {
-		super(DriverManager.getConnection(DB_URL, DB_USER, DB_PASS), SQLDialect.POSTGRES, new ConfiguredSettings());
+	public PostgresDSLContext() {
+		super(DATA_SOURCE, SQLDialect.POSTGRES, new ConfiguredSettings());
 	}
 
 	private static class ConfiguredSettings extends Settings {
