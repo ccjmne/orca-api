b846b6b564efe2b18135f275be487ceb02f190bf
ccjmne <ccjmne@gmail.com>
2015-11-24 23:37:37 +0100

    All the update queries are now executed within a transaction. Added methods to create/modify and delete departments. Sites can now have their primary key changed.

--- src/main/java/org/ccjmne/faomaintenance/api/rest/UpdateEndpoint.java
+++ src/main/java/org/ccjmne/faomaintenance/api/rest/UpdateEndpoint.java
@@ -1,5 +1,6 @@
 package org.ccjmne.faomaintenance.api.rest;
 
+import static org.ccjmne.faomaintenance.jooq.classes.Tables.DEPARTMENTS;
 import static org.ccjmne.faomaintenance.jooq.classes.Tables.EMPLOYEES;
 import static org.ccjmne.faomaintenance.jooq.classes.Tables.EMPLOYEES_ROLES;
 import static org.ccjmne.faomaintenance.jooq.classes.Tables.SITES;
@@ -41,10 +42,13 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.ccjmne.faomaintenance.api.utils.SQLDateFormat;
 import org.ccjmne.faomaintenance.jooq.classes.Sequences;
+import org.ccjmne.faomaintenance.jooq.classes.tables.records.SitesEmployeesRecord;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.jooq.DSLContext;
 import org.jooq.Field;
+import org.jooq.InsertValuesStep3;
+import org.jooq.impl.DSL;
 import org.jooq.tools.csv.CSVReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,12 +62,31 @@ public class UpdateEndpoint {
 	private final DSLContext ctx;
 	private final SQLDateFormat dateFormat;
 	private final StatisticsEndpoint statistics;
+	private final ResourcesEndpoint resources;
 
 	@Inject
-	public UpdateEndpoint(final DSLContext ctx, final SQLDateFormat dateFormat, final StatisticsEndpoint statistics) {
+	public UpdateEndpoint(final DSLContext ctx, final SQLDateFormat dateFormat, final StatisticsEndpoint statistics, final ResourcesEndpoint resources) {
 		this.dateFormat = dateFormat;
 		this.ctx = ctx;
 		this.statistics = statistics;
+		this.resources = resources;
+	}
+
+	@PUT
+	@Path("departments/{dept_pk}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public boolean updateDepartment(@PathParam("dept_pk") final Integer dept_pk, final Map<String, String> dept) {
+		if (this.ctx.fetchExists(DEPARTMENTS, DEPARTMENTS.DEPT_PK.eq(dept_pk))) {
+			this.ctx.update(DEPARTMENTS)
+					.set(DEPARTMENTS.DEPT_NAME, dept.get(DEPARTMENTS.DEPT_NAME.getName()))
+					.set(DEPARTMENTS.DEPT_ID, dept.get(DEPARTMENTS.DEPT_ID.getName()))
+					.where(DEPARTMENTS.DEPT_PK.eq(dept_pk)).execute();
+			return false;
+		}
+
+		this.ctx.insertInto(DEPARTMENTS, DEPARTMENTS.DEPT_PK, DEPARTMENTS.DEPT_NAME, DEPARTMENTS.DEPT_ID)
+				.values(dept_pk, dept.get(DEPARTMENTS.DEPT_NAME.getName()), dept.get(DEPARTMENTS.DEPT_ID.getName())).execute();
+		return true;
 	}
 
 	@PUT
@@ -71,8 +94,11 @@ public class UpdateEndpoint {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public boolean updateSite(@PathParam("site_pk") final String site_pk, final Map<String, String> site) {
 		if (this.ctx.fetchExists(SITES, SITES.SITE_PK.eq(site_pk))) {
-			this.ctx.update(SITES).set(SITES.SITE_NAME, site.get(SITES.SITE_NAME.getName()))
-					.set(SITES.SITE_DEPT_FK, Integer.valueOf(site.get(SITES.SITE_DEPT_FK.getName()))).where(SITES.SITE_PK.eq(site_pk)).execute();
+			this.ctx.update(SITES)
+					.set(SITES.SITE_NAME, site.get(SITES.SITE_NAME.getName()))
+					.set(SITES.SITE_DEPT_FK, Integer.valueOf(site.get(SITES.SITE_DEPT_FK.getName())))
+					.set(SITES.SITE_PK, site.getOrDefault(SITES.SITE_PK.getName(), site_pk))
+					.where(SITES.SITE_PK.eq(site_pk)).execute();
 			return false;
 		}
 
@@ -82,13 +108,31 @@ public class UpdateEndpoint {
 	}
 
 	@DELETE
+	@Path("departments/{dept_pk}")
+	public boolean deleteDept(@PathParam("dept_pk") final Integer dept_pk) {
+		final boolean exists = this.ctx.selectFrom(DEPARTMENTS).where(DEPARTMENTS.DEPT_PK.equal(dept_pk)).fetch().isNotEmpty();
+		if (exists) {
+			try {
+				this.resources.listSites(dept_pk, null, false).forEach(site -> deleteSite(site.getValue(SITES.SITE_PK)));
+			} catch (IllegalArgumentException | ParseException e) {
+				// Should *never* happen
+				e.printStackTrace();
+			}
+
+			this.ctx.delete(DEPARTMENTS).where(DEPARTMENTS.DEPT_PK.eq(dept_pk)).execute();
+		}
+
+		return exists;
+	}
+
+	@DELETE
 	@Path("sites/{site_pk}")
-	public boolean deleteSite(@PathParam("trng_pk") final String site_pk) {
+	public boolean deleteSite(@PathParam("site_pk") final String site_pk) {
 		final boolean exists = this.ctx.selectFrom(SITES).where(SITES.SITE_PK.equal(site_pk)).fetch().isNotEmpty();
 		if (exists) {
 			this.ctx.delete(SITES).where(SITES.SITE_PK.eq(site_pk)).execute();
 			this.ctx.delete(SITES_EMPLOYEES).where(SITES_EMPLOYEES.SIEM_SITE_FK.eq(site_pk)).execute();
-			this.statistics.invalidateSiteStats(Collections.singleton(site_pk));
+			this.statistics.invalidateSitesStats(Collections.singleton(site_pk));
 		}
 
 		return exists;
@@ -129,36 +173,49 @@ public class UpdateEndpoint {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response process(final List<Map<String, String>> employees) throws ParseException {
-		final Integer updt_pk = new Integer(this.ctx.nextval(Sequences.UPDATES_UPDT_PK_SEQ).intValue());
-		this.ctx.insertInto(UPDATES).set(UPDATES.UPDT_PK, updt_pk).set(UPDATES.UPDT_DATE, new java.sql.Date(new Date().getTime())).execute();
+	public Response process(final List<Map<String, String>> employees) {
+		try {
+			this.ctx.transaction(config -> {
+				final DSLContext transactionCtx = DSL.using(config);
+				final Integer updt_pk = new Integer(transactionCtx.nextval(Sequences.UPDATES_UPDT_PK_SEQ).intValue());
+				transactionCtx.insertInto(UPDATES).set(UPDATES.UPDT_PK, updt_pk).set(UPDATES.UPDT_DATE, new java.sql.Date(new Date().getTime())).execute();
 
-		for (final Map<String, String> employeeMap : employees) {
-			this.ctx.insertInto(SITES_EMPLOYEES, SITES_EMPLOYEES.SIEM_UPDT_FK, SITES_EMPLOYEES.SIEM_SITE_FK, SITES_EMPLOYEES.SIEM_EMPL_FK)
-					.values(updt_pk, employeeMap.get(SITES_EMPLOYEES.SIEM_SITE_FK.getName()), updateEmployee(employeeMap)).execute();
+				final InsertValuesStep3<SitesEmployeesRecord, Integer, String, String> query = transactionCtx
+						.insertInto(SITES_EMPLOYEES, SITES_EMPLOYEES.SIEM_UPDT_FK, SITES_EMPLOYEES.SIEM_SITE_FK, SITES_EMPLOYEES.SIEM_EMPL_FK);
+				for (final Map<String, String> employee : employees) {
+					query.values(updt_pk, employee.get(SITES_EMPLOYEES.SIEM_SITE_FK.getName()), updateEmployee(employee, transactionCtx));
+				}
+
+				query.execute();
+
+				// Remove all privileges of the remaining employees
+				transactionCtx
+						.delete(EMPLOYEES_ROLES)
+						.where(
+								EMPLOYEES_ROLES.EMPL_PK.notIn(transactionCtx.select(SITES_EMPLOYEES.SIEM_EMPL_FK).from(SITES_EMPLOYEES)
+										.where(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(updt_pk)))).execute();
+
+				// ... and set their site to #0 ('unassigned')
+				transactionCtx.insertInto(SITES_EMPLOYEES, SITES_EMPLOYEES.SIEM_EMPL_FK, SITES_EMPLOYEES.SIEM_SITE_FK, SITES_EMPLOYEES.SIEM_UPDT_FK)
+						.select(
+								transactionCtx.select(
+														SITES_EMPLOYEES.SIEM_EMPL_FK,
+														DSL.val("0"),
+														DSL.val(updt_pk)).from(SITES_EMPLOYEES)
+										.where(SITES_EMPLOYEES.SIEM_EMPL_FK
+												.notIn(transactionCtx.select(SITES_EMPLOYEES.SIEM_EMPL_FK).from(SITES_EMPLOYEES)
+														.where(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(updt_pk))))).execute();
+			});
+		} catch (final Exception e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
 		}
 
-		// Set site #0 ('unassigned') to remaining employees
-		// and remove all their privileges within this application.
-		this.ctx
-				.select(EMPLOYEES.EMPL_PK)
-				.from(EMPLOYEES)
-				.where(
-						EMPLOYEES.EMPL_PK.notIn(this.ctx.select(SITES_EMPLOYEES.SIEM_EMPL_FK).from(SITES_EMPLOYEES)
-								.where(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(updt_pk))))
-				.fetch(EMPLOYEES.EMPL_PK)
-				.forEach(
-							empl_pk -> {
-								this.ctx.delete(EMPLOYEES_ROLES).where(EMPLOYEES_ROLES.EMPL_PK.eq(empl_pk)).execute();
-								this.ctx.insertInto(SITES_EMPLOYEES, SITES_EMPLOYEES.SIEM_UPDT_FK, SITES_EMPLOYEES.SIEM_SITE_FK, SITES_EMPLOYEES.SIEM_EMPL_FK)
-										.values(updt_pk, "0", empl_pk).execute();
-							});
+		this.statistics.invalidateSitesStats();
 		return Response.ok().build();
 	}
 
 	@SuppressWarnings("unchecked")
-	private String updateEmployee(final Map<String, String> employee) throws ParseException {
+	private String updateEmployee(final Map<String, String> employee, final DSLContext context) throws ParseException {
 		final String empl_pk = employee.get(EMPLOYEES.EMPL_PK.getName());
 		final Map<Object, Object> record = new HashMap<>();
 		record.put(EMPLOYEES.EMPL_FIRSTNAME, employee.get(EMPLOYEES.EMPL_FIRSTNAME.getName()));
@@ -166,11 +223,12 @@ public class UpdateEndpoint {
 		record.put(EMPLOYEES.EMPL_DOB, this.dateFormat.parseSql(employee.get(EMPLOYEES.EMPL_DOB.getName())));
 		record.put(EMPLOYEES.EMPL_PERMANENT, Boolean.valueOf("CDI".equalsIgnoreCase(employee.get(EMPLOYEES.EMPL_PERMANENT.getName()))));
 		record.put(EMPLOYEES.EMPL_ADDR, employee.get(EMPLOYEES.EMPL_ADDR.getName()));
-		if (this.ctx.fetchExists(EMPLOYEES, EMPLOYEES.EMPL_PK.eq(empl_pk))) {
-			this.ctx.update(EMPLOYEES).set((Map<? extends Field<?>, ?>) record).where(EMPLOYEES.EMPL_PK.eq(empl_pk)).execute();
+
+		if (context.fetchExists(EMPLOYEES, EMPLOYEES.EMPL_PK.eq(empl_pk))) {
+			context.update(EMPLOYEES).set((Map<? extends Field<?>, ?>) record).where(EMPLOYEES.EMPL_PK.eq(empl_pk)).execute();
 		} else {
 			record.put(EMPLOYEES.EMPL_PK, empl_pk);
-			this.ctx.insertInto(EMPLOYEES).set((Map<? extends Field<?>, ?>) record).execute();
+			context.insertInto(EMPLOYEES).set((Map<? extends Field<?>, ?>) record).execute();
 		}
 
 		return empl_pk;
