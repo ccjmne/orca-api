3564a6a3c119abf06dbc88b4ea2b2c015ecc2b6c
ccjmne <ccjmne@gmail.com>
2017-04-12 23:58:34 +0200

    Added comments for employees that were flunked from a particular training.

--- src/main/java/org/ccjmne/faomaintenance/api/demo/DemoDataTrainings.java
+++ src/main/java/org/ccjmne/faomaintenance/api/demo/DemoDataTrainings.java
@@ -20,6 +20,7 @@ import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.jooq.Insert;
 import org.jooq.InsertValuesStep4;
+import org.jooq.Row1;
 import org.jooq.Table;
 import org.jooq.impl.DSL;
 
@@ -29,6 +30,7 @@ public class DemoDataTrainings {
 
 	public static final Fairy FAIRY = Fairy.create(Locale.FRENCH);
 
+	@SuppressWarnings("unchecked")
 	public static void generate(final DSLContext ctx) {
 		addCompletedTrainings(
 								ctx.insertInto(TRAININGS, TRAININGS.TRNG_DATE, TRAININGS.TRNG_START, TRAININGS.TRNG_TRTY_FK, TRAININGS.TRNG_OUTCOME),
@@ -75,21 +77,42 @@ public class DemoDataTrainings {
 		}
 
 		// Adding FLUNKED employees - 1/4 of them
-		ctx.insertInto(TRAININGS_EMPLOYEES, TRAININGS_EMPLOYEES.TREM_TRNG_FK, TRAININGS_EMPLOYEES.TREM_EMPL_FK, TRAININGS_EMPLOYEES.TREM_OUTCOME)
-				.select(DSL.select(DSL.field("trng_pk", Integer.class), DSL.field("empl_pk", String.class), DSL.val(Constants.EMPL_OUTCOME_FLUNKED))
-						.from(
-								DSL.select(
-											EMPLOYEES.EMPL_PK,
-											DSL.floor(DSL.rand().mul(DSL.select(DSL.count()).from(TRAININGS)
-													.where(TRAININGS.TRNG_OUTCOME.eq(Constants.TRNG_OUTCOME_COMPLETED))
-													.asField()).mul(DSL.val(4)))
-													.as("linked_trng_id"))
-										.from(EMPLOYEES).where(EMPLOYEES.EMPL_PK.ne(Constants.USER_ROOT)).asTable("employees2"))
-						.join(DSL.select(
-											TRAININGS.TRNG_PK,
-											DSL.rowNumber().over().orderBy(DSL.rand()).as("trng_id"))
-								.from(TRAININGS).where(TRAININGS.TRNG_OUTCOME.eq(Constants.TRNG_OUTCOME_COMPLETED)).asTable("trainings2"))
-						.on(DSL.field("linked_trng_id").eq(DSL.field("trng_id"))))
+		final Table<?> comments = DSL.values(Arrays.asList(
+															"En incapacité physique d'effectuer certains gestes techniques",
+															"Absent(e)",
+															"A quitté la formation avant la fin",
+															"En congés",
+															"En arrêt maladie",
+															"A été muté(e)")
+				.stream().map(DSL::row).toArray(Row1[]::new)).as("unused", "trem_comment");
+
+		ctx.insertInto(
+						TRAININGS_EMPLOYEES,
+						TRAININGS_EMPLOYEES.TREM_TRNG_FK,
+						TRAININGS_EMPLOYEES.TREM_EMPL_FK,
+						TRAININGS_EMPLOYEES.TREM_COMMENT,
+						TRAININGS_EMPLOYEES.TREM_OUTCOME)
+				.select(
+						DSL.select(
+									DSL.field("trng_pk", Integer.class),
+									DSL.field("empl_pk", String.class),
+									DSL.field("trem_comment", String.class),
+									DSL.val(Constants.EMPL_OUTCOME_FLUNKED))
+								.from(
+										DSL.select(
+													EMPLOYEES.EMPL_PK,
+													DSL.floor(DSL.rand().mul(DSL.select(DSL.count()).from(TRAININGS)
+															.where(TRAININGS.TRNG_OUTCOME.eq(Constants.TRNG_OUTCOME_COMPLETED))
+															.asField()).mul(DSL.val(4))).as("linked_trng_id"),
+													DSL.ceil(DSL.rand().mul(DSL.select(DSL.count()).from(comments).asField())).as("linked_trem_comment_id"))
+												.from(EMPLOYEES).where(EMPLOYEES.EMPL_PK.ne(Constants.USER_ROOT)).asTable("employees2"))
+								.join(DSL.select(
+													TRAININGS.TRNG_PK,
+													DSL.rowNumber().over().orderBy(DSL.rand()).as("trng_id"))
+										.from(TRAININGS).where(TRAININGS.TRNG_OUTCOME.eq(Constants.TRNG_OUTCOME_COMPLETED)).asTable("trainings2"))
+								.on(DSL.field("linked_trng_id").eq(DSL.field("trng_id")))
+								.join(DSL.select(DSL.field("trem_comment"), DSL.rowNumber().over().as("trem_comment_id")).from(comments))
+								.on(DSL.field("linked_trem_comment_id").eq(DSL.field("trem_comment_id"))))
 				.execute();
 
 		// Adding PENDING employees - 1/35 of them
@@ -133,11 +156,11 @@ public class DemoDataTrainings {
 									Constants.TRNG_OUTCOME_SCHEDULED));
 	}
 
-	private static <R> Field<R> random(final Table<?> table, final Field<R> field, final Condition... conditions) {
+	public static <R> Field<R> random(final Table<?> table, final Field<R> field, final Condition... conditions) {
 		return DSL.select(field).from(table).where(conditions).orderBy(DSL.rand()).limit(1).asField();
 	}
 
-	private static List<? extends Field<?>> asFields(final Object... values) {
+	public static List<? extends Field<?>> asFields(final Object... values) {
 		return Arrays.asList(values).stream().map(v -> v instanceof Field<?> ? (Field<?>) v : DSL.val(v)).collect(Collectors.toList());
 	}
 }
