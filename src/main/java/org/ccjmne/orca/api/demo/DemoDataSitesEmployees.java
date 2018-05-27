6c9c72efddf0b8e42b01f060604db8fdac9e3b6e
ccjmne <ccjmne@gmail.com>
2018-05-27 17:52:55 +0200

    More code style update

--- src/main/java/org/ccjmne/orca/api/demo/DemoDataSitesEmployees.java
+++ src/main/java/org/ccjmne/orca/api/demo/DemoDataSitesEmployees.java
@@ -11,6 +11,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -32,24 +33,33 @@ public class DemoDataSitesEmployees {
 		ctx.batchInsert(DemoDataSitesEmployees.addEmployees(5000)).execute();
 
 		// GEO tags
-		DemoDataSitesEmployees.insertTags(	ctx, ctx.insertInto(TAGS, TAGS.TAGS_NAME, TAGS.TAGS_SHORT, TAGS.TAGS_TYPE, TAGS.TAGS_HEX_COLOUR)
-				.values("Situation Géographique", "GEO", Constants.TAGS_TYPE_STRING, "#C71585").returning(TAGS.TAGS_PK).fetchOne().get(TAGS.TAGS_PK),
-											"ABCDEF".chars()
-													.mapToObj(c -> String.format("Zone %s", Character.valueOf((char) c)))
-													.map(DSL::row)
-													.toArray(Row1[]::new));
+		ctx.execute(DemoDataSitesEmployees
+				.insertTags(
+							ctx.insertInto(TAGS, TAGS.TAGS_NAME, TAGS.TAGS_SHORT, TAGS.TAGS_TYPE, TAGS.TAGS_HEX_COLOUR)
+									.values("Situation Géographique", "GEO", Constants.TAGS_TYPE_STRING, "#C71585").returning(TAGS.TAGS_PK).fetchOne()
+									.get(TAGS.TAGS_PK),
+							"ABCDEF".chars()
+									.mapToObj(c -> String.format("Zone %s", Character.valueOf((char) c)))
+									.map(DSL::row)
+									.toArray(Row1[]::new)));
 
 		// TYPE tags
-		DemoDataSitesEmployees.insertTags(	ctx, ctx.insertInto(TAGS, TAGS.TAGS_NAME, TAGS.TAGS_SHORT, TAGS.TAGS_TYPE, TAGS.TAGS_HEX_COLOUR)
-				.values("Type d'Activité", "TYPE", Constants.TAGS_TYPE_STRING, "#795548").returning(TAGS.TAGS_PK).fetchOne().get(TAGS.TAGS_PK),
-											Arrays.asList("Usine", "Bureaux", "Entrepôt", "Boutique").stream().map(DSL::row).toArray(Row1[]::new));
+		ctx.execute(DemoDataSitesEmployees
+				.insertTags(
+							ctx.insertInto(TAGS, TAGS.TAGS_NAME, TAGS.TAGS_SHORT, TAGS.TAGS_TYPE, TAGS.TAGS_HEX_COLOUR)
+									.values("Type d'Activité", "TYPE", Constants.TAGS_TYPE_STRING, "#795548").returning(TAGS.TAGS_PK).fetchOne()
+									.get(TAGS.TAGS_PK),
+							Arrays.asList("Usine", "Bureaux", "Entrepôt", "Boutique").stream().map(DSL::row).toArray(Row1[]::new)));
 
 		// ERP tags
-		DemoDataSitesEmployees.insertTags(	ctx, ctx.insertInto(TAGS, TAGS.TAGS_NAME, TAGS.TAGS_SHORT, TAGS.TAGS_TYPE, TAGS.TAGS_HEX_COLOUR)
-				.values("Établissement Recevant du Public", "ERP", Constants.TAGS_TYPE_BOOLEAN, "#009688").returning(TAGS.TAGS_PK).fetchOne().get(TAGS.TAGS_PK),
-											Arrays.asList(String.valueOf(Boolean.TRUE), String.valueOf(Boolean.FALSE)).stream()
-													.map(DSL::row)
-													.toArray(Row1[]::new));
+		ctx.execute(DemoDataSitesEmployees
+				.insertTags(
+							ctx.insertInto(TAGS, TAGS.TAGS_NAME, TAGS.TAGS_SHORT, TAGS.TAGS_TYPE, TAGS.TAGS_HEX_COLOUR)
+									.values("Établissement Recevant du Public", "ERP", Constants.TAGS_TYPE_BOOLEAN, "#009688")
+									.returning(TAGS.TAGS_PK).fetchOne().get(TAGS.TAGS_PK),
+							Arrays.asList(String.valueOf(Boolean.TRUE), String.valueOf(Boolean.FALSE)).stream()
+									.map(DSL::row)
+									.toArray(Row1[]::new)));
 
 		// Update dated from NCLS Development's birth day :)
 		final Integer update = ctx.insertInto(UPDATES, UPDATES.UPDT_DATE).values(Date.valueOf(LocalDate.of(2014, Month.DECEMBER, 8))).returning(UPDATES.UPDT_PK)
@@ -72,23 +82,16 @@ public class DemoDataSitesEmployees {
 				.execute();
 	}
 
-	public static void insertTags(final DSLContext ctx, final Integer tags_pk, final Row1<String>[] values) {
+	public static Insert<SitesTagsRecord> insertTags(final Integer tags_pk, final Row1<String>[] values) {
 		final Table<Record1<String>> tagsTable = DSL.values(values).asTable();
-		ctx.insertInto(SITES_TAGS, SITES_TAGS.SITA_SITE_FK, SITES_TAGS.SITA_TAGS_FK, SITES_TAGS.SITA_VALUE)
-				.select(DSL
-						.select(DSL.field("site", Integer.class), DSL.val(tags_pk), DSL.field("tag", String.class))
-						.from(DSL.select(
-											SITES.SITE_PK.as("site"),
-											DSL
-													.rowNumber().over().orderBy(DSL.rand())
-													.mod(Integer.valueOf(values.length))
-													.plus(Integer.valueOf(1))
-													.as("tag_fk"))
-								.from(SITES)
-								.where(SITES.SITE_PK.ne(Constants.UNASSIGNED_SITE)))
+		return DSL.insertInto(SITES_TAGS, SITES_TAGS.SITA_SITE_FK, SITES_TAGS.SITA_TAGS_FK, SITES_TAGS.SITA_VALUE)
+				.select(DSL.select(DSL.field("site", Integer.class), DSL.val(tags_pk), DSL.field("tag", String.class))
+						.from(DSL
+								.select(SITES.SITE_PK.as("site"),
+										DSL.rowNumber().over().orderBy(DSL.rand()).mod(Integer.valueOf(values.length)).plus(Integer.valueOf(1)).as("tag_fk"))
+								.from(SITES).where(SITES.SITE_PK.ne(Constants.UNASSIGNED_SITE)))
 						.join(DSL.select(tagsTable.field(0, String.class).as("tag"), DSL.rowNumber().over().as("tag_pk")).from(tagsTable))
-						.on(DSL.field("tag_fk").eq(DSL.field("tag_pk"))))
-				.execute();
+						.on(DSL.field("tag_fk").eq(DSL.field("tag_pk"))));
 	}
 
 	private static List<EmployeesRecord> addEmployees(final int amount) {
