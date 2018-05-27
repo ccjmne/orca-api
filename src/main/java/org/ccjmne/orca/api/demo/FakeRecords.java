fecab3292c679ab926d4fe555ca573ee11d1caa9
ccjmne <ccjmne@gmail.com>
2018-05-27 15:05:43 +0200

    Enhance FakeRecords w/ training sessions data table generation method

--- src/main/java/org/ccjmne/orca/api/demo/FakeRecords.java
+++ src/main/java/org/ccjmne/orca/api/demo/FakeRecords.java
@@ -1,14 +1,26 @@
 package org.ccjmne.orca.api.demo;
 
+import static org.ccjmne.orca.jooq.classes.Tables.TRAININGTYPES;
+
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
+import org.ccjmne.orca.api.utils.Constants;
 import org.ccjmne.orca.jooq.classes.tables.records.EmployeesRecord;
 import org.ccjmne.orca.jooq.classes.tables.records.SitesRecord;
+import org.jooq.Condition;
+import org.jooq.Field;
+import org.jooq.Record4;
+import org.jooq.Row4;
+import org.jooq.Table;
+import org.jooq.impl.DSL;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -25,6 +37,9 @@ public class FakeRecords {
 	private final List<String> cities;
 
 	private final Range<LocalDate> dobRange = Range.closed(LocalDate.now().minusYears(60), LocalDate.now().minusYears(20));
+	private final Range<LocalDate> cancelledTraining = Range.closed(LocalDate.now().minusYears(4), LocalDate.now());
+	private final Range<LocalDate> completedTraining = Range.closed(LocalDate.now().minusYears(4), LocalDate.now());
+	private final Range<LocalDate> scheduledTraining = Range.closed(LocalDate.now(), LocalDate.now().plusMonths(6));
 
 	public static SitesRecord randomSite(final int primaryKey) {
 		return INSTANCE.site(Integer.valueOf(primaryKey));
@@ -32,6 +47,10 @@ public class FakeRecords {
 
 	public static EmployeesRecord randomEmployee(final int primaryKey) {
 		return INSTANCE.employee(Integer.valueOf(primaryKey));
+	}
+
+	public static Table<Record4<Integer, Date, String, String>> randomSessions(final String outcome, final int amount) {
+		return INSTANCE.sessions(outcome, amount);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -74,6 +93,31 @@ public class FakeRecords {
 									String.format("empl-%04d", primaryKey));
 	}
 
+	@SuppressWarnings("unchecked")
+	private Table<Record4<Integer, Date, String, String>> sessions(final String outcome, final int amount) {
+		return DSL.values(IntStream.range(0, amount)
+				.mapToObj(i -> {
+					final Date date;
+					switch (outcome) {
+						case Constants.TRNG_OUTCOME_CANCELLED:
+							date = FakeRecords.anyFrom(this.cancelledTraining);
+							break;
+						case Constants.TRNG_OUTCOME_COMPLETED:
+							date = FakeRecords.anyFrom(this.completedTraining);
+							break;
+						case Constants.TRNG_OUTCOME_SCHEDULED:
+							date = FakeRecords.anyFrom(this.scheduledTraining);
+							break;
+						default:
+							// Can *not* happen
+							throw new IllegalArgumentException(String.format("The outcome of a training must be one of %s.", Constants.TRAINING_OUTCOMES));
+					}
+
+					return DSL.row(FakeRecords.asFields(FakeRecords.random(TRAININGTYPES, TRAININGTYPES.TRTY_PK), date, outcome, ""));
+				})
+				.toArray(Row4[]::new));
+	}
+
 	private static String asEmail(final String userName) {
 		return String.format(	"%s@demo.orca-solution.com",
 								StringUtils
@@ -89,5 +133,13 @@ public class FakeRecords {
 	private static Date anyFrom(final Range<LocalDate> dateRange) {
 		return Date.valueOf(LocalDate
 				.ofEpochDay(RANDOM.nextLong(dateRange.lowerEndpoint().toEpochDay(), dateRange.upperEndpoint().toEpochDay())));
+	}
+
+	private static List<? extends Field<?>> asFields(final Object... values) {
+		return Arrays.asList(values).stream().map(v -> v instanceof Field<?> ? (Field<?>) v : DSL.val(v)).collect(Collectors.toList());
+	}
+
+	private static <R> Field<R> random(final Table<?> table, final Field<R> field, final Condition... conditions) {
+		return DSL.select(field).from(table).where(conditions).orderBy(DSL.rand()).limit(1).asField();
 	}
 }
