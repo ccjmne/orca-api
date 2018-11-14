925fb8d59db5b315fb1eb16d85380bbca1d5f1d4
ccjmne <ccjmne@gmail.com>
2018-11-14 19:24:09 +0100

    WIP: Delegate resources and statistics JSON manipulation to the DB

--- src/main/java/org/ccjmne/orca/api/utils/RestrictedResourcesAccess.java
+++ src/main/java/org/ccjmne/orca/api/utils/RestrictedResourcesAccess.java
@@ -4,20 +4,15 @@ import static org.ccjmne.orca.jooq.classes.Tables.EMPLOYEES;
 import static org.ccjmne.orca.jooq.classes.Tables.SITES;
 import static org.ccjmne.orca.jooq.classes.Tables.SITES_EMPLOYEES;
 import static org.ccjmne.orca.jooq.classes.Tables.SITES_TAGS;
+import static org.ccjmne.orca.jooq.classes.Tables.TAGS;
 import static org.ccjmne.orca.jooq.classes.Tables.TRAININGS_EMPLOYEES;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.ForbiddenException;
 
+import org.ccjmne.orca.api.modules.QueryParameters;
 import org.ccjmne.orca.api.modules.RecordsCollator;
 import org.ccjmne.orca.api.modules.Restrictions;
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
-import org.jooq.Condition;
 import org.jooq.JoinType;
 import org.jooq.Record;
 import org.jooq.SelectQuery;
@@ -32,105 +27,86 @@ import org.jooq.impl.DSL;
  */
 public class RestrictedResourcesAccess {
 
+	private final QueryParameters parameters;
 	private final Restrictions restrictions;
 	private final RecordsCollator recordsCollator;
 
 	@Inject
-	public RestrictedResourcesAccess(final Restrictions restrictions, final RecordsCollator recordsCollator) {
+	public RestrictedResourcesAccess(final QueryParameters parameters, final Restrictions restrictions, final RecordsCollator recordsCollator) {
+		this.parameters = parameters;
 		this.restrictions = restrictions;
 		this.recordsCollator = recordsCollator;
 	}
 
 	/**
-	 * Unassigned employees should only ever be accessed through their training
-	 * sessions, since they only need to keep existing there for history
-	 * purposes.<br />
+	 * "Retired" employees (i.e.: those that aren't assigned to any site as per
+	 * the most relevant update) should only ever be accessed through their
+	 * training sessions, since they only need to keep existing there for
+	 * history purposes.<br />
 	 * Thus, employees that aren't assigned to any site can be accessed if and
 	 * only if:
 	 * <ul>
-	 * <li><code>tagFilters</code> is <code>empty</code>, and</li>
-	 * <li><code>site_pk</code> is <code>null</code>, and</li>
-	 * <li><code>trng_pk</code> is <strong>defined</strong></li>
+	 * <li>Fetching a specific employee ({@code has(QueryParameters.EMPLOYEE)}),
+	 * and:</li>
+	 * <li>{@link Restrictions#canAccessTrainings()} is {@code true}</li>
 	 * </ul>
 	 * Or:
 	 * <ul>
-	 * <li><code>empl_pk</code> is <strong>defined</strong>, and</li>
-	 * <li>{@link Restrictions#canAccessTrainings()} is <code>true</code></li>
+	 * <li>Fetching a specific training session
+	 * ({@code has(QueryParameters.SESSION)})</li>
 	 * </ul>
 	 */
-	public boolean accessUnassignedEmployees(final Integer empl_pk, final Integer site_pk, final Integer trng_pk, final Map<Integer, List<String>> tagFilters) {
-		if ((empl_pk != null) && this.restrictions.canAccessTrainings()) {
-			return true;
-		}
-
-		return tagFilters.isEmpty() && (site_pk == null) && (trng_pk != null);
+	public boolean includeRetiredEmployees() {
+		return this.parameters.has(QueryParameters.SESSION) || (this.parameters.has(QueryParameters.EMPLOYEE) && this.restrictions.canAccessTrainings());
 	}
 
-	public SelectQuery<Record> selectEmployees(
-												@Nullable final Integer empl_pk,
-												@Nullable final Integer site_pk,
-												@Nullable final Integer trng_pk,
-												@Nullable final String dateStr,
-												@NonNull final Map<Integer, List<String>> tagFilters) {
+	public SelectQuery<Record> selectEmployees() {
 		try (final SelectQuery<Record> query = DSL.select().getQuery()) {
 			query.addFrom(EMPLOYEES);
 			query.addConditions(EMPLOYEES.EMPL_PK.ne(Constants.EMPLOYEE_ROOT));
+			// TODO: Use DSL.noCondition() when upgrading jOOQ
 			query.addJoin(
 							SITES_EMPLOYEES,
-							this.accessUnassignedEmployees(empl_pk, site_pk, trng_pk, tagFilters) ? JoinType.LEFT_OUTER_JOIN : JoinType.JOIN,
 							SITES_EMPLOYEES.SIEM_EMPL_FK.eq(EMPLOYEES.EMPL_PK),
-							SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(dateStr)),
-							SITES_EMPLOYEES.SIEM_SITE_FK.in(Constants.select(SITES.SITE_PK, this.selectSites(site_pk, tagFilters))));
+							SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(this.parameters.of(QueryParameters.DATE))),
+							DSL.or(	SITES_EMPLOYEES.SIEM_SITE_FK.in(Constants.select(SITES.SITE_PK, this.selectSites())),
+									this.includeRetiredEmployees() ? SITES_EMPLOYEES.SIEM_SITE_FK.isNull() : DSL.falseCondition()));
 
-			if (trng_pk != null) {
+			if (this.parameters.has(QueryParameters.SESSION)) {
 				if (!this.restrictions.canAccessTrainings()) {
 					throw new ForbiddenException();
 				}
 
-				query.addJoin(TRAININGS_EMPLOYEES, TRAININGS_EMPLOYEES.TREM_EMPL_FK.eq(EMPLOYEES.EMPL_PK), TRAININGS_EMPLOYEES.TREM_TRNG_FK.eq(trng_pk));
+				query.addJoin(	TRAININGS_EMPLOYEES, TRAININGS_EMPLOYEES.TREM_EMPL_FK.eq(EMPLOYEES.EMPL_PK),
+								TRAININGS_EMPLOYEES.TREM_TRNG_FK.eq(this.parameters.get(QueryParameters.SESSION)));
 			}
 
-			if (empl_pk != null) {
-				query.addConditions(EMPLOYEES.EMPL_PK.eq(empl_pk));
+			if (this.parameters.has(QueryParameters.EMPLOYEE)) {
+				query.addConditions(EMPLOYEES.EMPL_PK.eq(this.parameters.get(QueryParameters.EMPLOYEE)));
 			}
 
 			return this.recordsCollator.applyFAndS(query);
 		}
 	}
 
-	/**
-	 * Selects sites for which:
-	 * <ul>
-	 * <li><code>site_pk</code> (if specified) uniquely identifies it,
-	 * <em>and</em></li>
-	 * <li><strong>for each</strong> filter
-	 * <code>{ k: [v1, v2, ..., vN] }</code>:
-	 * <ul>
-	 * <li>the site has a tag <code>{ type, value }</code> where:
-	 * <ul>
-	 * <li><code>type == k</code>, and</li>
-	 * <li><code>[v1, v2, ..., vN].contains(value)</code></li>
-	 * </ul>
-	 * </li>
-	 * <li>or, <strong>iff
-	 * <code>[v1, v2, ..., vN].contains({@link Constants.TAGS_VALUE_NONE})</code></strong>:<br/>
-	 * the site has <strong>no</strong> tag <code>{ type, value }</code> where
-	 * <code>type == k</code></li>
-	 * </ul>
-	 * </li>
-	 * </ul>
-	 *
-	 * @param site_pk
-	 *            Optional. The identifier of the only site to select
-	 * @param filters
-	 *            Non-null. Map of tag types and values to satisfy for the sites
-	 *            to be selected
-	 */
-	public SelectQuery<Record> selectSites(final Integer site_pk, final Map<Integer, List<String>> filters) {
+	public SelectQuery<Record> selectSites() {
 		try (final SelectQuery<Record> query = DSL.select().getQuery()) {
+			query.addSelect(SITES.fields());
 			query.addFrom(SITES);
 			query.addConditions(SITES.SITE_PK.ne(Constants.DECOMMISSIONED_SITE));
-			if ((site_pk == null) && !this.restrictions.canAccessAllSites()) {
+			query.addSelect(DSL.count(SITES_EMPLOYEES.SIEM_EMPL_FK).as("site_employees_count"));
+			query.addSelect(DSL.count(SITES_EMPLOYEES.SIEM_EMPL_FK).filterWhere(EMPLOYEES.EMPL_PERMANENT.eq(Boolean.TRUE)).as("site_permanent_count"));
+			query.addJoin(	SITES_EMPLOYEES.join(EMPLOYEES).on(EMPLOYEES.EMPL_PK.eq(SITES_EMPLOYEES.SIEM_EMPL_FK)),
+							this.parameters.is(QueryParameters.INCLUDE_DECOMISSIONED, Boolean.TRUE) ? JoinType.LEFT_OUTER_JOIN : JoinType.JOIN,
+							SITES_EMPLOYEES.SIEM_SITE_FK.eq(SITES.SITE_PK));
+
+			if (this.parameters.has(QueryParameters.SITE)) {
+				if (!this.restrictions.canAccessSite(this.parameters.getRaw(QueryParameters.SITE))) {
+					throw new ForbiddenException();
+				}
+
+				query.addConditions(SITES.SITE_PK.eq(this.parameters.get(QueryParameters.SITE)));
+			} else if (!this.restrictions.canAccessAllSites()) {
 				if (this.restrictions.getAccessibleSites().isEmpty()) {
 					throw new ForbiddenException();
 				}
@@ -138,37 +114,15 @@ public class RestrictedResourcesAccess {
 				query.addConditions(SITES.SITE_PK.in(this.restrictions.getAccessibleSites()));
 			}
 
-			if (!filters.isEmpty()) {
-				if (!this.restrictions.canAccessSitesWith(filters)) {
-					throw new ForbiddenException();
-				}
+			query.addGroupBy(SITES.fields());
 
-				query.addConditions(filters.entrySet().stream()
-						.map(tag -> {
-							final Condition hasCorrectValue = DSL.exists(DSL.selectZero().from(SITES_TAGS)
-									.where(SITES_TAGS.SITA_SITE_FK.eq(SITES.SITE_PK))
-									.and(SITES_TAGS.SITA_TAGS_FK.eq(tag.getKey()))
-									.and(SITES_TAGS.SITA_VALUE.in(tag.getValue())));
-
-							if (tag.getValue().contains(Constants.TAGS_VALUE_NONE)) {
-								return hasCorrectValue.or(DSL.notExists(DSL.selectZero().from(SITES_TAGS)
-										.where(SITES_TAGS.SITA_SITE_FK.eq(SITES.SITE_PK))
-										.and(SITES_TAGS.SITA_TAGS_FK.eq(tag.getKey()))));
-							}
-
-							return hasCorrectValue;
-						}).collect(Collectors.toList()));
-			}
-
-			if (site_pk != null) {
-				if (!this.restrictions.canAccessSite(site_pk)) {
-					throw new ForbiddenException();
-				}
-
-				query.addConditions(SITES.SITE_PK.eq(site_pk));
-			}
-
-			return this.recordsCollator.applyFAndS(query);
+			return this.recordsCollator.applyFAndS(DSL
+					.select(query.fields())
+					.select(ResourcesHelper.jsonbObjectAggNullSafe(SITES_TAGS.SITA_TAGS_FK, ResourcesHelper.TAG_VALUE_COERCED).as("site_tags"))
+					.from(query)
+					.leftOuterJoin(SITES_TAGS).on(SITES_TAGS.SITA_SITE_FK.eq(query.field(SITES.SITE_PK)))
+					.join(TAGS).on(TAGS.TAGS_PK.eq(SITES_TAGS.SITA_TAGS_FK)) // In order to extract TAGS_TYPE for TAG_VALUE_COERCED
+					.groupBy(query.fields()));
 		}
 	}
 }
--- src/main/java/org/ccjmne/orca/api/utils/StatisticsHelper.java
+++ src/main/java/org/ccjmne/orca/api/utils/StatisticsHelper.java
@@ -2,9 +2,7 @@ package org.ccjmne.orca.api.utils;
 
 import static org.ccjmne.orca.jooq.classes.Tables.CERTIFICATES;
 import static org.ccjmne.orca.jooq.classes.Tables.EMPLOYEES_VOIDINGS;
-import static org.ccjmne.orca.jooq.classes.Tables.SITES;
 import static org.ccjmne.orca.jooq.classes.Tables.SITES_EMPLOYEES;
-import static org.ccjmne.orca.jooq.classes.Tables.SITES_TAGS;
 import static org.ccjmne.orca.jooq.classes.Tables.TRAININGS;
 import static org.ccjmne.orca.jooq.classes.Tables.TRAININGS_EMPLOYEES;
 import static org.ccjmne.orca.jooq.classes.Tables.TRAININGS_TRAINERS;
@@ -13,11 +11,14 @@ import static org.ccjmne.orca.jooq.classes.Tables.TRAININGTYPES_CERTIFICATES;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.util.Optional;
 
+import javax.inject.Inject;
+
+import org.ccjmne.orca.api.modules.QueryParameters;
 import org.eclipse.jdt.annotation.NonNull;
-import org.jooq.Condition;
 import org.jooq.Field;
-import org.jooq.JoinType;
+import org.jooq.Param;
 import org.jooq.Record;
 import org.jooq.SelectFinalStep;
 import org.jooq.SelectQuery;
@@ -30,24 +31,18 @@ public class StatisticsHelper {
 
 	private static final Integer DURATION_INFINITE = Integer.valueOf(0);
 
-	private static final Field<Date> MAX_EXPIRY = DSL.max(DSL
+	public static final Field<Date> MAX_EXPIRY = DSL.max(DSL
 			.when(TRAININGTYPES_CERTIFICATES.TTCE_DURATION.eq(StatisticsHelper.DURATION_INFINITE), DSL.date(Constants.DATE_INFINITY))
 			.otherwise(TRAININGS.TRNG_DATE.plus(TRAININGTYPES_CERTIFICATES.TTCE_DURATION.mul(new YearToMonth(0, 1)))));
 
 	private static final Field<Date> EXPIRY = DSL
-			.when(
-					EMPLOYEES_VOIDINGS.EMVO_DATE.le(StatisticsHelper.MAX_EXPIRY),
-					EMPLOYEES_VOIDINGS.EMVO_DATE.sub(new DayToSecond(1)))
+			.when(EMPLOYEES_VOIDINGS.EMVO_DATE.le(StatisticsHelper.MAX_EXPIRY), EMPLOYEES_VOIDINGS.EMVO_DATE.sub(new DayToSecond(1)))
 			.otherwise(StatisticsHelper.MAX_EXPIRY);
 
-	private static Field<String> fieldValidity(final String dateStr) {
+	private static Field<String> fieldValidity(final Optional<Param<Date>> date) {
 		return DSL
-				.when(
-						StatisticsHelper.EXPIRY.ge(Constants.fieldDate(dateStr).plus(new YearToMonth(0, 6))),
-						Constants.STATUS_SUCCESS)
-				.when(
-						StatisticsHelper.EXPIRY.ge(Constants.fieldDate(dateStr)),
-						Constants.STATUS_WARNING)
+				.when(StatisticsHelper.EXPIRY.ge(Constants.fieldDate(date).plus(new YearToMonth(0, 6))), Constants.STATUS_SUCCESS)
+				.when(StatisticsHelper.EXPIRY.ge(Constants.fieldDate(date)), Constants.STATUS_WARNING)
 				.otherwise(Constants.STATUS_DANGER);
 	}
 
@@ -55,51 +50,22 @@ public class StatisticsHelper {
 		return DSL.field(EMPLOYEES_VOIDINGS.EMVO_DATE);
 	}
 
-	/**
-	 * The <code>Condition</code> should be on
-	 * <code>TRAININGS_EMPLOYEES.TREM_EMPL_FK</code>.
-	 */
-	public static SelectFinalStep<? extends Record> selectEmployeesStats(
-																			final String dateStr,
-																			final Condition employeesSelection) {
-		return DSL
-				.select(
-						SITES_EMPLOYEES.SIEM_SITE_FK,
-						TRAININGS_EMPLOYEES.TREM_EMPL_FK,
-						TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK,
-						StatisticsHelper.EXPIRY.as("expiry"),
-						StatisticsHelper.fieldOptedOut().as("opted_out"),
-						StatisticsHelper.fieldValidity(dateStr).as("validity"))
-				.from(TRAININGTYPES_CERTIFICATES)
-				.join(TRAININGTYPES).on(TRAININGTYPES.TRTY_PK.eq(TRAININGTYPES_CERTIFICATES.TTCE_TRTY_FK))
-				.join(TRAININGS).on(TRAININGS.TRNG_TRTY_FK.eq(TRAININGTYPES.TRTY_PK))
-				.join(TRAININGS_EMPLOYEES).on(TRAININGS_EMPLOYEES.TREM_TRNG_FK.eq(TRAININGS.TRNG_PK))
-				.leftJoin(EMPLOYEES_VOIDINGS)
-				.on(EMPLOYEES_VOIDINGS.EMVO_EMPL_FK.eq(TRAININGS_EMPLOYEES.TREM_EMPL_FK)
-						.and(EMPLOYEES_VOIDINGS.EMVO_CERT_FK.eq(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK)))
-				.join(SITES_EMPLOYEES)
-				.on(SITES_EMPLOYEES.SIEM_EMPL_FK.eq(TRAININGS_EMPLOYEES.TREM_EMPL_FK)
-						.and(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(dateStr))))
-				.where(TRAININGS_EMPLOYEES.TREM_OUTCOME.eq(Constants.EMPL_OUTCOME_VALIDATED))
-				.and(TRAININGS.TRNG_DATE.le(Constants.fieldDate(dateStr)))
-				.and(employeesSelection)
-				.groupBy(
-							SITES_EMPLOYEES.SIEM_SITE_FK,
-							TRAININGS_EMPLOYEES.TREM_EMPL_FK,
-							TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK,
-							EMPLOYEES_VOIDINGS.EMVO_DATE);
+	private final Optional<Param<Date>> date;
+
+	@Inject
+	public StatisticsHelper(final QueryParameters parameters) {
+		this.date = parameters.of(QueryParameters.DATE);
 	}
 
-	// TODO: Return a {@link Table}
-	public static SelectFinalStep<? extends Record> selectEmployeesStats(final String dateStr) {
+	public SelectFinalStep<? extends Record> selectEmployeesStats() {
 		return DSL
 				.select(
 						SITES_EMPLOYEES.SIEM_SITE_FK,
 						TRAININGS_EMPLOYEES.TREM_EMPL_FK,
 						TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK,
 						ResourcesHelper.formatDate(StatisticsHelper.EXPIRY).as("expiry"),
-						StatisticsHelper.fieldOptedOut().as("opted_out"),
-						StatisticsHelper.fieldValidity(dateStr).as("validity"))
+						StatisticsHelper.fieldOptedOut().as("void_since"),
+						StatisticsHelper.fieldValidity(this.date).as("status"))
 				.from(TRAININGTYPES_CERTIFICATES)
 				.join(TRAININGTYPES).on(TRAININGTYPES.TRTY_PK.eq(TRAININGTYPES_CERTIFICATES.TTCE_TRTY_FK))
 				.join(TRAININGS).on(TRAININGS.TRNG_TRTY_FK.eq(TRAININGTYPES.TRTY_PK))
@@ -109,9 +75,9 @@ public class StatisticsHelper {
 						.and(EMPLOYEES_VOIDINGS.EMVO_CERT_FK.eq(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK)))
 				.join(SITES_EMPLOYEES)
 				.on(SITES_EMPLOYEES.SIEM_EMPL_FK.eq(TRAININGS_EMPLOYEES.TREM_EMPL_FK)
-						.and(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(dateStr))))
+						.and(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(this.date))))
 				.where(TRAININGS_EMPLOYEES.TREM_OUTCOME.eq(Constants.EMPL_OUTCOME_VALIDATED))
-				.and(TRAININGS.TRNG_DATE.le(Constants.fieldDate(dateStr)))
+				.and(TRAININGS.TRNG_DATE.le(Constants.fieldDate(this.date)))
 				.groupBy(
 							SITES_EMPLOYEES.SIEM_SITE_FK,
 							TRAININGS_EMPLOYEES.TREM_EMPL_FK,
@@ -119,139 +85,103 @@ public class StatisticsHelper {
 							EMPLOYEES_VOIDINGS.EMVO_DATE);
 	}
 
-	/**
-	 * The <code>employeesSelection</code> condition should be on
-	 * <code>TRAININGS_EMPLOYEES.TREM_EMPL_FK</code>.<br />
-	 * The <code>sitesSelection</code> condition should be on
-	 * <code>SITES_EMPLOYEES.SIEM_SITE_FK</code>.
-	 */
 	@SuppressWarnings("null")
-	public static SelectFinalStep<? extends Record> selectSitesStats(
-																		final String dateStr,
-																		final Condition employeesSelection,
-																		final Condition sitesSelection) {
-		final Table<? extends Record> employeesStats = StatisticsHelper
-				.selectEmployeesStats(dateStr, employeesSelection)
+	public SelectFinalStep<? extends Record> selectSitesStats() {
+		final Table<? extends Record> eStats = this.selectEmployeesStats().asTable();
+		final Field<String> eStatus = eStats.field("status", String.class);
+		final Table<? extends Record> sStats = DSL
+				.select(
+						eStats.field(SITES_EMPLOYEES.SIEM_SITE_FK),
+						eStats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK),
+						DSL.count().filterWhere(eStatus.eq(Constants.STATUS_SUCCESS)).as(Constants.STATUS_SUCCESS),
+						DSL.count().filterWhere(eStatus.eq(Constants.STATUS_WARNING)).as(Constants.STATUS_WARNING),
+						DSL.count().filterWhere(eStatus.eq(Constants.STATUS_DANGER)).as(Constants.STATUS_DANGER))
+				.from(eStats)
+				.groupBy(eStats.field(SITES_EMPLOYEES.SIEM_SITE_FK), eStats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK))
 				.asTable();
 
-		final Table<? extends Record> certificatesStats = DSL
+		final Field<Integer> eCount = DSL.count().as("site_employees");
+		final Table<? extends Record> certs = DSL
 				.select(
-						employeesStats.field(SITES_EMPLOYEES.SIEM_SITE_FK),
-						employeesStats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK),
-						DSL.count(employeesStats.field(TRAININGS_EMPLOYEES.TREM_EMPL_FK))
-								.filterWhere(employeesStats.field("validity", String.class).eq(Constants.STATUS_SUCCESS))
-								.as(Constants.STATUS_SUCCESS),
-						DSL.count(employeesStats.field(TRAININGS_EMPLOYEES.TREM_EMPL_FK))
-								.filterWhere(employeesStats.field("validity", String.class).eq(Constants.STATUS_WARNING))
-								.as(Constants.STATUS_WARNING),
-						DSL.count(employeesStats.field(TRAININGS_EMPLOYEES.TREM_EMPL_FK))
-								.filterWhere(employeesStats.field("validity", String.class).eq(Constants.STATUS_DANGER))
-								.as(Constants.STATUS_DANGER))
-				.from(employeesStats)
-				.groupBy(employeesStats.field(SITES_EMPLOYEES.SIEM_SITE_FK), employeesStats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK))
-				.asTable();
-
-		final Table<? extends Record> certificates = DSL
-				.select(
-						SITES_EMPLOYEES.SIEM_SITE_FK,
 						CERTIFICATES.CERT_PK,
 						CERTIFICATES.CERT_TARGET,
-						DSL.count().as("employeesCount"))
-				.from(SITES_EMPLOYEES)
-				.rightJoin(CERTIFICATES).on(DSL.val(true))
-				.where(sitesSelection)
-				.and(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(dateStr)))
-				.groupBy(SITES_EMPLOYEES.SIEM_SITE_FK, CERTIFICATES.CERT_PK, CERTIFICATES.CERT_TARGET)
+						SITES_EMPLOYEES.SIEM_SITE_FK,
+						eCount)
+				.from(CERTIFICATES)
+				.join(SITES_EMPLOYEES).on(DSL.trueCondition())
+				// TODO: Use DSL.noCondition() when upgrading jOOQ
+				.and(SITES_EMPLOYEES.SIEM_UPDT_FK.eq(Constants.selectUpdate(this.date)))
+				.groupBy(CERTIFICATES.CERT_PK, SITES_EMPLOYEES.SIEM_SITE_FK)
 				.asTable();
 
-		final Field<Integer> targetCount = DSL.ceil(certificates.field("employeesCount", Integer.class)
-				.mul(certificates.field(CERTIFICATES.CERT_TARGET).div(DSL.val(100f))));
-		final Field<Integer> warningTargetCount = DSL.ceil(certificates.field("employeesCount", Integer.class)
-				.mul(certificates.field(CERTIFICATES.CERT_TARGET).div(DSL.val(300 / 2f))));
-		final Field<Integer> validCount = DSL
-				.coalesce(
-							certificatesStats.field(Constants.STATUS_SUCCESS, Integer.class).add(certificatesStats.field(Constants.STATUS_WARNING)),
+		final Field<Integer> target = DSL.ceil(eCount.mul(certs.field(CERTIFICATES.CERT_TARGET).div(DSL.val(100f))));
+		final Field<Integer> warningTarget = DSL.ceil(eCount.mul(certs.field(CERTIFICATES.CERT_TARGET).div(DSL.val(300 / 2f))));
+		final Field<Integer> current = DSL
+				.coalesce(	sStats.field(Constants.STATUS_SUCCESS, Integer.class).add(sStats.field(Constants.STATUS_WARNING)),
 							Integer.valueOf(0));
 		return DSL.select(
-							certificates.field(SITES_EMPLOYEES.SIEM_SITE_FK),
-							certificates.field(CERTIFICATES.CERT_PK),
-							DSL.coalesce(certificatesStats.field(Constants.STATUS_SUCCESS, Integer.class), Integer.valueOf(0)).as(Constants.STATUS_SUCCESS),
-							DSL.coalesce(certificatesStats.field(Constants.STATUS_WARNING, Integer.class), Integer.valueOf(0)).as(Constants.STATUS_WARNING),
-							DSL.coalesce(certificatesStats.field(Constants.STATUS_DANGER, Integer.class), Integer.valueOf(0)).as(Constants.STATUS_DANGER),
-							validCount.as("count"),
-							targetCount.as("target"),
+							certs.field(eCount),
+							certs.field(SITES_EMPLOYEES.SIEM_SITE_FK),
+							certs.field(CERTIFICATES.CERT_PK),
+							DSL.coalesce(sStats.field(Constants.STATUS_SUCCESS, Integer.class), Integer.valueOf(0)).as(Constants.STATUS_SUCCESS),
+							DSL.coalesce(sStats.field(Constants.STATUS_WARNING, Integer.class), Integer.valueOf(0)).as(Constants.STATUS_WARNING),
+							DSL.coalesce(sStats.field(Constants.STATUS_DANGER, Integer.class), Integer.valueOf(0)).as(Constants.STATUS_DANGER),
+							current.as("current"),
+							target.as("target"),
 							DSL
-									.when(validCount.ge(targetCount), Constants.STATUS_SUCCESS)
-									.when(validCount.ge(warningTargetCount), Constants.STATUS_WARNING)
-									.otherwise(Constants.STATUS_DANGER).as("validity"))
-				.from(certificatesStats)
-				.rightJoin(certificates)
-				.on(certificates.field(CERTIFICATES.CERT_PK).eq(certificatesStats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK))
-						.and(certificates.field(SITES_EMPLOYEES.SIEM_SITE_FK).eq(certificatesStats.field(SITES_EMPLOYEES.SIEM_SITE_FK))))
-				.join(SITES).on(SITES.SITE_PK.eq(certificates.field(SITES_EMPLOYEES.SIEM_SITE_FK)));
+									.when(current.ge(target), Constants.STATUS_SUCCESS)
+									.when(current.ge(warningTarget), Constants.STATUS_WARNING)
+									.otherwise(Constants.STATUS_DANGER).as("status"))
+				.from(certs)
+				.leftOuterJoin(sStats)
+				.on(sStats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK).eq(certs.field(CERTIFICATES.CERT_PK))
+						.and(sStats.field(SITES_EMPLOYEES.SIEM_SITE_FK).eq(certs.field(SITES_EMPLOYEES.SIEM_SITE_FK))));
 	}
 
 	/**
-	 * The <code>employeesSelection</code> condition should be on
-	 * <code>TRAININGS_EMPLOYEES.TREM_EMPL_FK</code>.<br />
-	 * The <code>sitesSelection</code> condition should be on
-	 * <code>SITES_EMPLOYEES.SIEM_SITE_FK</code>.<br />
-	 * The <code>tagsSelection</code> condition should be on <code>
-	 * SITES_TAGS.SITA_TAGS_FK</code>
+	 * <strong>Warning:</strong> This one's a special one. It can't be
+	 * {@code JOIN}'d to a
+	 * "sites-groups"-yielding query, since doing so would grant no way of only
+	 * selecting certain sites within those groups.<br />
+	 * <br />
+	 * For that reason, the query built with this method, ran by itself, would build
+	 * the statistics for all the selected data, as one universal blob.<br />
+	 * <br />
+	 * It needs to be {@code JOIN}'d to the relevant sites first (for filtering),
+	 * then actually {@code GROUP BY}'d the sites-group identifier.
 	 */
 	@SuppressWarnings("null")
-	public static SelectQuery<? extends Record> selectSitesGroupsStats(
-																		final String dateStr,
-																		final Condition employeesSelection,
-																		final Condition sitesSelection,
-																		final Integer tags_pk) {
-		final Table<? extends Record> sitesStats = StatisticsHelper
-				.selectSitesStats(dateStr, employeesSelection, sitesSelection)
-				.asTable();
+	public SelectQuery<? extends Record> selectSitesGroupsStats() {
+		final Table<? extends Record> sitesStats = this.selectSitesStats().asTable();
 
-		final Field<@NonNull String> validityStatus = sitesStats.field("validity", String.class);
+		final Field<@NonNull String> status = sitesStats.field("status", String.class);
 		final Field<BigDecimal> score = DSL.round(DSL
 				.sum(DSL
-						.when(validityStatus.eq(Constants.STATUS_SUCCESS), DSL.val(1f))
-						.when(validityStatus.eq(Constants.STATUS_WARNING), DSL.val(2 / 3f))
+						.when(status.eq(Constants.STATUS_SUCCESS), DSL.val(1f))
+						.when(status.eq(Constants.STATUS_WARNING), DSL.val(2 / 3f))
 						.otherwise(DSL.val(0f)))
 				.mul(DSL.val(100)).div(DSL.count()));
 
 		try (final SelectQuery<Record> q = DSL.select().getQuery()) {
 			q.addSelect(sitesStats.field(CERTIFICATES.CERT_PK));
 			q.addSelect(
-						DSL.sum(sitesStats.field("count", Integer.class)).as("count"),
+						DSL.sum(sitesStats.field("current", Integer.class)).as("current"),
+						DSL.sum(sitesStats.field("target", Integer.class)).as("target"),
+						score.as("score"),
 						DSL.sum(sitesStats.field(Constants.STATUS_SUCCESS, Integer.class)).as(Constants.STATUS_SUCCESS),
 						DSL.sum(sitesStats.field(Constants.STATUS_WARNING, Integer.class)).as(Constants.STATUS_WARNING),
 						DSL.sum(sitesStats.field(Constants.STATUS_DANGER, Integer.class)).as(Constants.STATUS_DANGER),
-						DSL.count().filterWhere(validityStatus.eq(Constants.STATUS_SUCCESS))
+						DSL.count().filterWhere(status.eq(Constants.STATUS_SUCCESS))
 								.as("sites_" + Constants.STATUS_SUCCESS),
-						DSL.count().filterWhere(validityStatus.eq(Constants.STATUS_WARNING))
+						DSL.count().filterWhere(status.eq(Constants.STATUS_WARNING))
 								.as("sites_" + Constants.STATUS_WARNING),
-						DSL.count().filterWhere(validityStatus.eq(Constants.STATUS_DANGER))
+						DSL.count().filterWhere(status.eq(Constants.STATUS_DANGER))
 								.as("sites_" + Constants.STATUS_DANGER),
-						score.as("score"),
 						DSL.when(score.eq(DSL.val(BigDecimal.valueOf(100))), Constants.STATUS_SUCCESS)
 								.when(score.ge(DSL.val(BigDecimal.valueOf(67))), Constants.STATUS_WARNING)
-								.otherwise(Constants.STATUS_DANGER).as("validity"));
-
+								.otherwise(Constants.STATUS_DANGER).as("status"));
 			q.addFrom(sitesStats);
-
-			if (tags_pk != null) {
-				// Non-tagged sites appear under TAGS_VALUE_NONE
-				q.addSelect(ResourcesHelper.coalesce(SITES_TAGS.SITA_VALUE, Constants.TAGS_VALUE_NONE));
-				q.addJoin(
-							SITES_TAGS,
-							JoinType.LEFT_OUTER_JOIN,
-							SITES_TAGS.SITA_SITE_FK.eq(sitesStats.field(SITES_EMPLOYEES.SIEM_SITE_FK))
-									.and(SITES_TAGS.SITA_TAGS_FK.eq(tags_pk)));
-			} else {
-				// All sites marked as TAGS_VALUE_UNIVERSAL
-				q.addSelect(DSL.val(Constants.TAGS_VALUE_UNIVERSAL).as(SITES_TAGS.SITA_VALUE));
-				q.addJoin(SITES_TAGS, JoinType.LEFT_OUTER_JOIN, DSL.condition(Boolean.FALSE));
-			}
-
-			q.addGroupBy(SITES_TAGS.SITA_VALUE, sitesStats.field(CERTIFICATES.CERT_PK));
+			q.addGroupBy(sitesStats.field(CERTIFICATES.CERT_PK));
 			return q;
 		}
 	}
