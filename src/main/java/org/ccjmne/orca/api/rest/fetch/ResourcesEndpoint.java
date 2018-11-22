46040e4827bfd3d8da4719805d6fd48bf65e68fb
ccjmne <ccjmne@gmail.com>
2018-11-22 19:31:01 +0100

    Rewrite core resources listing module
    Use paginated responses for all listing endpoints.
    Break trainings statistics generation.

--- src/main/java/org/ccjmne/orca/api/rest/fetch/ResourcesEndpoint.java
+++ src/main/java/org/ccjmne/orca/api/rest/fetch/ResourcesEndpoint.java
@@ -11,13 +11,11 @@ import static org.ccjmne.orca.jooq.classes.Tables.UPDATES;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 import org.ccjmne.orca.api.modules.QueryParameters;
 import org.ccjmne.orca.api.modules.RecordsCollator;
-import org.ccjmne.orca.api.modules.Restrictions;
 import org.ccjmne.orca.api.utils.Constants;
 import org.ccjmne.orca.api.utils.ResourcesHelper;
 import org.ccjmne.orca.api.utils.ResourcesSelection;
@@ -35,8 +33,8 @@ import org.jooq.impl.DSL;
 import com.fasterxml.jackson.databind.JsonNode;
 
 /**
- * Serves the resources whose access is restricted based on the request's
- * associated {@link Restrictions}.<br />
+ * Serves the core resources, whose access is restricted on a per-request
+ * level.
  *
  * @author ccjmne
  */
@@ -49,20 +47,14 @@ public class ResourcesEndpoint {
   private final RecordsCollator     collator;
   private final QueryParameters     parameters;
 
-  // TODO: Should not have any use for this and should delegate restricted
-  // data access mechanics to RestrictedResourcesHelper
-  private final Restrictions restrictions;
-
   @Inject
   public ResourcesEndpoint(
                            final DSLContext ctx,
-                           final Restrictions restrictions,
                            final ResourcesSelection resourcesSelection,
                            final StatisticsSelection statisticsSelection,
                            final RecordsCollator collator,
                            final QueryParameters parameters) {
     this.ctx = ctx;
-    this.restrictions = restrictions;
     this.resourcesSelection = resourcesSelection;
     this.statisticsSelection = statisticsSelection;
     this.collator = collator;
@@ -94,169 +86,135 @@ public class ResourcesEndpoint {
    * Needs to be a {@code Map} instead of a {@code List}.
    */
 
-  /*
+  /**
    * EMPLOYEES listing methods
    * ------------------------------------------------------------------------
-   * GET /employees
-   * GET /employees/{employee}
-   * GET /sites/{site}/employees
-   * GET /sessions/{session}/trainees
+   *
+   * <pre>
+   * +--------+------------------------------+-----------+
+   * | Method | Path                         | Response  |
+   * +--------+------------------------------+-----------+
+   * | GET    | /employees                   | PAGINATED |
+   * +--------+------------------------------+-----------+
+   * | GET    | /employees/{employee}        | SINGLE    |
+   * +--------+------------------------------+-----------+
+   * | GET    | /sites/{site}/employees      | PAGINATED |
+   * +--------+------------------------------+-----------+
+   * | GET    | /sessions/{session}/trainees | PAGINATED |
+   * +--------+------------------------------+-----------+
+   * </pre>
    */
 
   @GET
   @Path("employees")
-  public Result<? extends Record> listEmployees() {
-    final Table<? extends Record> employees = this.resourcesSelection.selectEmployees().asTable();
-    final Table<? extends Record> stats = this.statisticsSelection.selectEmployeesStats().asTable();
-    return this.ctx.fetch(this.collator.applyAll(DSL
-        .select(employees.fields())
-        .select(ResourcesHelper
-            .jsonbObjectAggNullSafe(stats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK), stats.fields("status", "expiry", "void_since"))
-            .as("empl_stats"))
-        .from(employees)
-        .leftOuterJoin(stats).on(stats.field(TRAININGS_EMPLOYEES.TREM_EMPL_FK).eq(employees.field(EMPLOYEES.EMPL_PK)))
-        .groupBy(employees.fields()).getQuery()));
+  public Record listEmployees() {
+    return this.ctx.fetchOne(this.collator.applyPagination(this.findEmployees()));
   }
 
   @GET
   @Path("employees/{employee}")
   public Record lookupEmployee() {
-    try {
-      return this.listEmployees().get(0);
-    } catch (final IndexOutOfBoundsException e) {
-      // TODO: Maybe handle with an ExceptionMapper
-      throw new NotFoundException();
-    }
+    return this.ctx.fetchOne(this.findEmployees());
   }
 
   @GET
   @Path("sites/{site}/employees")
-  public Result<? extends Record> listSiteEmployees() {
+  public Record listSiteEmployees() {
     return this.listEmployees();
   }
 
   @GET
   @Path("sessions/{session}/trainees")
-  public Result<? extends Record> listTrainees() {
+  public Record listTrainees() {
     return this.listEmployees();
   }
 
-  /*
+  /**
    * SITES listing methods
    * ------------------------------------------------------------------------
-   * GET /sites
-   * GET /sites/{site}
+   *
+   * <pre>
+   * +--------+---------------+-----------+
+   * | Method | Path          | Response  |
+   * +--------+---------------+-----------+
+   * | GET    | /sites        | PAGINATED |
+   * +--------+---------------+-----------+
+   * | GET    | /sites/{site} | SINGLE    |
+   * +--------+---------------+-----------+
+   * </pre>
    */
 
   @GET
   @Path("sites")
-  public Result<? extends Record> listSites() {
-    final Table<? extends Record> sites = this.resourcesSelection.selectSites().asTable();
-    final Table<? extends Record> stats = this.statisticsSelection.selectSitesStats().asTable();
-    return this.ctx.fetch(this.collator.applyAll(DSL
-        .select(sites.fields())
-        .select(ResourcesHelper.jsonbObjectAggNullSafe(stats.field(CERTIFICATES.CERT_PK),
-                                                       stats.fields("status", "current", "target", "success", "warning", "danger"))
-            .as("site_stats"))
-        .from(sites)
-        .leftOuterJoin(stats).on(stats.field(SITES_EMPLOYEES.SIEM_SITE_FK).eq(sites.field(SITES.SITE_PK)))
-        .groupBy(sites.fields())));
+  public Record listSites() {
+    return this.ctx.fetchOne(this.collator.applyPagination(this.findSites()));
   }
 
   @GET
   @Path("sites/{site}")
   public Record lookupSite() {
-    try {
-      return this.listSites().get(0);
-    } catch (final IndexOutOfBoundsException e) {
-      // TODO: Maybe handle with an ExceptionMapper
-      throw new NotFoundException();
-    }
+    return this.ctx.fetchOne(this.findSites());
   }
 
-  /*
+  /**
    * SITES-GROUPS listing methods
    * ------------------------------------------------------------------------
-   * GET /sites-groups
-   * GET /sites-groups/{group-by}
+   *
+   * <pre>
+   * +--------+--------------------------+-----------+
+   * | Method | Path                     | Response  |
+   * +--------+--------------------------+-----------+
+   * | GET    | /sites-groups            | PAGINATED |
+   * +--------+--------------------------+-----------+
+   * | GET    | /sites-groups/{group-by} | PAGINATED |
+   * +--------+--------------------------+-----------+
+   * </pre>
    */
 
   @GET
   @Path("sites-groups")
-  public Result<? extends Record> listSitesGroups() {
-    final Table<? extends Record> sites = this.resourcesSelection.selectSites().asTable();
-    final Field<JsonNode> groupID = this.parameters.get(QueryParameters.GROUP_BY_FIELD).as("sgrp_value");
-    final Table<? extends Record> groups = DSL
-        .select(DSL.sum(sites.field("site_employees_count", Integer.class)).as("sgrp_employees_count"))
-        .select(DSL.sum(sites.field("site_permanent_count", Integer.class)).as("sgrp_permanent_count"))
-        .select(DSL.count(sites.field(SITES.SITE_PK)).as("sgrp_sites_count"))
-        .select(groupID)
-        .from(sites)
-        .groupBy(groupID).asTable();
-
-    try (final SelectQuery<? extends Record> stats = this.statisticsSelection.selectSitesGroupsStats()) {
-      stats.addSelect(groupID);
-      stats.addJoin(sites,
-                    JoinType.RIGHT_OUTER_JOIN,
-                    sites.field(SITES.SITE_PK).eq(Constants.unqualify(SITES_EMPLOYEES.SIEM_SITE_FK)));
-      stats.addGroupBy(groupID);
-
-      return this.ctx.fetch(this.collator.applyAll(DSL
-          .select(groups.fields())
-          .select(ResourcesHelper.jsonbObjectAggNullSafe(stats.field(CERTIFICATES.CERT_PK), stats
-              .fields("status", "current", "target", "score", Constants.STATUS_SUCCESS, Constants.STATUS_WARNING, Constants.STATUS_DANGER,
-                      "sites_" + Constants.STATUS_SUCCESS, "sites_" + Constants.STATUS_WARNING, "sites_" + Constants.STATUS_DANGER))
-              .as("sgrp_stats"))
-          .from(groups)
-          .leftOuterJoin(stats).on(stats.field(groupID).eq(groups.field(groupID))
-              .or(stats.field(groupID).isNull().and(groups.field(groupID).isNull())))
-          .groupBy(groups.fields())));
-    }
+  public Record listSitesGroups() {
+    return this.ctx.fetchOne(this.collator.applyPagination(this.findSitesGroups()));
   }
 
   @GET
   @Path("sites-groups/{group-by}")
-  public Result<? extends Record> listSitesGroupedBy() {
+  public Record listSitesGroupedBy() {
     return this.listSitesGroups();
   }
 
-  /*
+  /**
    * SESSIONS listing methods
    * ------------------------------------------------------------------------
-   * GET /sessions
-   * GET /sessions/{session}
-   * GET /employees/{employee}/sessions
+   *
+   * <pre>
+   * +--------+--------------------------------+-----------+
+   * | Method | Path                           | Response  |
+   * +--------+--------------------------------+-----------+
+   * | GET    | /sessions                      | PAGINATED |
+   * +--------+--------------------------------+-----------+
+   * | GET    | /sessions/{session}            | SINGLE    |
+   * +--------+--------------------------------+-----------+
+   * | GET    | /employees/{employee}/sessions | PAGINATED |
+   * +--------+--------------------------------+-----------+
+   * </pre>
    */
 
   @GET
   @Path("sessions")
-  public Result<Record> listSessions() {
-    final Table<Record> sessions = this.resourcesSelection.selectSessions().asTable();
-    final Table<? extends Record> stats = StatisticsSelection.selectSessionsStats().asTable();
-
-    return this.ctx.fetch(this.collator.applyAll(DSL.select(sessions.fields())
-        .select(ResourcesHelper.jsonbObjectAggNullSafe(stats.field("outcome"), stats.field("count")).as("stats"))
-        .select(DSL.sum(stats.field("count", Integer.class)).as("trainees_count"))
-        .from(sessions)
-        .leftOuterJoin(stats)
-        .on(stats.field(TRAININGS_EMPLOYEES.TREM_TRNG_FK).eq(sessions.field(TRAININGS.TRNG_PK)))
-        .groupBy(sessions.fields())));
+  public Record listSessions() {
+    return this.ctx.fetchOne(this.collator.applyPagination(this.findSessions()));
   }
 
   @GET
   @Path("sessions/{session}")
   public Record lookupSession() {
-    try {
-      return this.listSessions().get(0);
-    } catch (final IndexOutOfBoundsException e) {
-      // TODO: Maybe handle with an ExceptionMapper
-      throw new NotFoundException();
-    }
+    return this.ctx.fetchOne(this.collator.applyFAndS(this.findSessions()));
   }
 
   @GET
   @Path("employees/{employee}/sessions")
-  public Result<Record> lookupEmployeeSessions() {
+  public Record lookupEmployeeSessions() {
     return this.listSessions();
   }
 
@@ -272,5 +230,79 @@ public class ResourcesEndpoint {
   // TODO: move to UpdateEndpoint?
   public Record lookupUpdate(@PathParam("date") final String dateStr) {
     return this.ctx.selectFrom(UPDATES).where(UPDATES.UPDT_PK.eq(Constants.selectUpdate(dateStr))).fetchAny();
+  }
+
+  /**
+   * Actual implementations
+   * ------------------------------------------------------------------------
+   */
+
+  private SelectQuery<Record> findEmployees() {
+    final Table<? extends Record> employees = this.resourcesSelection.selectEmployees().asTable();
+    final Table<? extends Record> stats = this.statisticsSelection.selectEmployeesStats().asTable();
+
+    return this.collator.applyFAndS(DSL
+        .select(employees.fields())
+        .select(ResourcesHelper
+            .jsonbObjectAggNullSafe(stats.field(TRAININGTYPES_CERTIFICATES.TTCE_CERT_FK), stats.fields("status", "expiry", "void_since"))
+            .as("empl_stats"))
+        .from(employees)
+        .leftOuterJoin(stats).on(stats.field(TRAININGS_EMPLOYEES.TREM_EMPL_FK).eq(employees.field(EMPLOYEES.EMPL_PK)))
+        .groupBy(employees.fields()));
+  }
+
+  private SelectQuery<Record> findSites() {
+    final Table<? extends Record> sites = this.resourcesSelection.selectSites().asTable();
+    final Table<? extends Record> stats = this.statisticsSelection.selectSitesStats().asTable();
+
+    return this.collator.applyFAndS(DSL
+        .select(sites.fields())
+        .select(ResourcesHelper
+            .jsonbObjectAggNullSafe(stats.field(CERTIFICATES.CERT_PK), stats.fields("status", "current", "target", "success", "warning", "danger"))
+            .as("site_stats"))
+        .from(sites)
+        .leftOuterJoin(stats).on(stats.field(SITES_EMPLOYEES.SIEM_SITE_FK).eq(sites.field(SITES.SITE_PK)))
+        .groupBy(sites.fields()));
+  }
+
+  private SelectQuery<Record> findSitesGroups() {
+    final Table<? extends Record> sites = this.resourcesSelection.selectSites().asTable();
+    final Field<JsonNode> groupID = this.parameters.get(QueryParameters.GROUP_BY_FIELD).as("sgrp_value");
+    final Table<? extends Record> groups = DSL
+        .select(DSL.sum(sites.field("site_employees_count", Integer.class)).as("sgrp_employees_count"))
+        .select(DSL.sum(sites.field("site_permanent_count", Integer.class)).as("sgrp_permanent_count"))
+        .select(DSL.count(sites.field(SITES.SITE_PK)).as("sgrp_sites_count"))
+        .select(groupID)
+        .from(sites)
+        .groupBy(groupID).asTable();
+
+    try (final SelectQuery<? extends Record> stats = this.statisticsSelection.selectSitesGroupsStats()) {
+      stats.addSelect(groupID);
+      stats.addJoin(sites, JoinType.RIGHT_OUTER_JOIN, sites.field(SITES.SITE_PK).eq(Constants.unqualify(SITES_EMPLOYEES.SIEM_SITE_FK)));
+      stats.addGroupBy(groupID);
+
+      return this.collator.applyFAndS(DSL
+          .select(groups.fields())
+          .select(ResourcesHelper.jsonbObjectAggNullSafe(stats.field(CERTIFICATES.CERT_PK), stats
+              .fields("status", "current", "target", "score", "success", "warning", "danger", "sites_success", "sites_warning", "sites_danger"))
+              .as("sgrp_stats"))
+          .from(groups)
+          .leftOuterJoin(stats).on(stats.field(groupID).eq(groups.field(groupID)).or(stats.field(groupID).isNull().and(groups.field(groupID).isNull())))
+          .groupBy(groups.fields()));
+    }
+  }
+
+  private SelectQuery<Record> findSessions() {
+    final Table<Record> sessions = this.resourcesSelection.selectSessions().asTable();
+    final Table<? extends Record> stats = StatisticsSelection.selectSessionsStats().asTable();
+
+    return this.collator.applyFAndS(DSL
+        .select(sessions.fields())
+        .select(ResourcesHelper.jsonbObjectAggNullSafe(stats.field("outcome"), stats.field("count")).as("stats"))
+        .select(DSL.sum(stats.field("count", Integer.class)).as("trainees_count"))
+        .from(sessions)
+        .leftOuterJoin(stats)
+        .on(stats.field(TRAININGS_EMPLOYEES.TREM_TRNG_FK).eq(sessions.field(TRAININGS.TRNG_PK)))
+        .groupBy(sessions.fields()));
   }
 }
