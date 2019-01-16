542a575860aa2552ae4e8957b57108e384f3abf0
ccjmne <ccjmne@gmail.com>
2019-01-16 14:46:57 +0100

    Extend QueryParameter's CustomType

--- src/main/java/org/ccjmne/orca/api/inject/business/QueryParameters.java
+++ src/main/java/org/ccjmne/orca/api/inject/business/QueryParameters.java
@@ -18,10 +18,11 @@ import javax.ws.rs.core.UriInfo;
 import org.ccjmne.orca.api.utils.Constants;
 import org.ccjmne.orca.api.utils.JSONFields;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.jooq.Field;
 import org.jooq.Param;
 import org.jooq.impl.DSL;
-import org.jooq.util.postgres.PostgresDataType;
+import org.jooq.impl.SQLDataType;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -31,20 +32,22 @@ import com.fasterxml.jackson.databind.JsonNode;
  */
 public class QueryParameters {
 
-  public static final Type<Integer>               EMPLOYEE              = new Type<>("employee", Integer.class);
-  public static final Type<Integer>               SITE                  = new Type<>("site", Integer.class);
-  public static final Type<Integer>               SESSION               = new Type<>("session", Integer.class);
-  public static final Type<Integer>               CERTIFICATE           = new Type<>("certificate", Integer.class);
-  public static final Type<Boolean>               INCLUDE_DECOMISSIONED = new Type<>("include-decommissioned", Boolean.class);
-  public static final CustomType<String>          INTERVAL              = new CustomType<>("interval", value -> value, "month");
-  public static final CustomType<Field<Date>>     FROM                  = new CustomType<>("from", value -> DSL.val(value, Date.class), DSL.currentDate());
-  public static final CustomType<Field<Date>>     TO                    = new CustomType<>("to", value -> DSL.val(value, Date.class), DSL.currentDate());
-  public static final CustomType<Field<Date>>     DATE                  = new CustomType<>("date", value -> DSL.val(value, Date.class), DSL.currentDate());
-  public static final CustomType<Field<JsonNode>> GROUP_BY_FIELD        = new CustomType<>("group-by", value -> DSL
-      .field("site_tags -> {0}", JSONFields.JSON_TYPE, value), JSONFields.toJson(DSL.cast(Constants.TAGS_VALUE_UNIVERSAL, PostgresDataType.TEXT)));
+  public static final Type<Integer> EMPLOYEE              = new Type<>("employee", Integer.class);
+  public static final Type<Integer> SITE                  = new Type<>("site", Integer.class);
+  public static final Type<Integer> SESSION               = new Type<>("session", Integer.class);
+  public static final Type<Integer> CERTIFICATE           = new Type<>("certificate", Integer.class);
+  public static final Type<Boolean> INCLUDE_DECOMISSIONED = new Type<>("include-decommissioned", Boolean.class);
+  public static final Type<String>  SEARCH_TERMS          = new Type<>("q", String.class);
 
-  private final Map<Type<?>, Param<?>>     types;
-  private final Map<CustomType<?>, Object> customTypes;
+  public static final FirstParamType<String>          INTERVAL       = new FirstParamType<>("interval", v -> v, "month");
+  public static final FirstParamType<Field<Date>>     FROM           = new FirstParamType<>("from", v -> DSL.val(v, Date.class), DSL.currentDate());
+  public static final FirstParamType<Field<Date>>     TO             = new FirstParamType<>("to", v -> DSL.val(v, Date.class), DSL.currentDate());
+  public static final FirstParamType<Field<Date>>     DATE           = new FirstParamType<>("date", v -> DSL.val(v, Date.class), DSL.currentDate());
+  public static final FirstParamType<Field<JsonNode>> GROUP_BY_FIELD = new FirstParamType<>("group-by", v -> DSL
+      .field("site_tags -> {0}", JSONFields.JSON_TYPE, v), JSONFields.toJson(DSL.cast(Constants.TAGS_VALUE_UNIVERSAL, SQLDataType.VARCHAR)));
+
+  private final Map<Type<?>, Param<?>>        types;
+  private final Map<CustomType<?, ?>, Object> customTypes;
 
   /**
    * Parses the request URL for arguments.<br />
@@ -88,7 +91,7 @@ public class QueryParameters {
     return this.get(type).getValue();
   }
 
-  public <T> Optional<T> of(final CustomType<T> type) {
+  public <T> Optional<T> of(final CustomType<?, T> type) {
     return Optional.ofNullable(this.get(type));
   }
 
@@ -97,23 +100,26 @@ public class QueryParameters {
    *
    * @see QueryParameters#isDefault(CustomType)
    */
-  public <T> boolean has(final CustomType<T> type) {
+  public <T> boolean has(final CustomType<?, T> type) {
     return this.of(type).isPresent();
   }
 
   /**
-   * Only {@code true} when the given {@code type} does have a default value that
-   * wasn't overridden by the query parameters.
+   * Is {@code true} when the given {@code type} does have a default value that
+   * either matches or wasn't overridden by the query parameters.
    */
-  public <T> boolean isDefault(final CustomType<T> type) {
+  public <T> boolean isDefault(final CustomType<?, T> type) {
     return (type.orElse != null) && !this.customTypes.containsKey(type);
   }
 
   @SuppressWarnings("unchecked") // always safe
-  public <T> T get(final CustomType<T> type) {
+  public <T> T get(final CustomType<?, T> type) {
     return (T) this.customTypes.getOrDefault(type, type.orElse);
   }
 
+  // TODO: Document
+  // TODO: Rename?
+  // TODO: extend CustomType?
   private static class Type<T> {
 
     private static final Map<String, Type<?>> TYPES = new HashMap<>();
@@ -135,27 +141,79 @@ public class QueryParameters {
     }
   }
 
-  private static class CustomType<T> {
+  /**
+   * Like a {@link Type}, except that:
+   * <ul>
+   * <li>it can coerce parameters to any arbitrary {@code T} class</li>
+   * <li>it can have a default value</li>
+   * </ul>
+   *
+   * @param <U>
+   *          For internal use only.
+   * @param <T>
+   *          The coerced value for this {@code CustomType}
+   * @see FirstParamType
+   * @see AllParamsType
+   */
+  // TODO: Rename?
+  private static abstract class CustomType<U, T> {
 
-    private static final Map<String, CustomType<?>> TYPES = new HashMap<>();
+    private static final Map<String, CustomType<?, ?>> TYPES = new HashMap<>();
 
-    protected final T orElse;
+    protected final @Nullable T orElse;
 
-    private final Function<? super String, ? extends T> coercer;
+    private final Function<? super U, ? extends T> coercer;
 
-    protected CustomType(final String name, final Function<? super String, ? extends T> coercer) {
-      this(name, coercer, null);
-    }
-
-    protected CustomType(final String name, final Function<? super String, ? extends T> coercer, final T orElse) {
+    protected CustomType(final String name, final Function<? super U, ? extends T> coercer, @Nullable final T orElse) {
       this.coercer = coercer;
       this.orElse = orElse;
+      // TODO: throw if replacing existing entry
       TYPES.put(name, this);
     }
 
-    protected static Optional<Map.Entry<CustomType<?>, ?>> mapper(final Map.Entry<String, List<String>> source) {
-      final CustomType<?> t = TYPES.get(source.getKey());
-      return t != null ? Optional.of(new SimpleEntry<>(t, t.coercer.apply(source.getValue().get(0)))) : Optional.empty();
+    protected static <U, T> Optional<Map.Entry<CustomType<U, T>, T>> mapper(final Map.Entry<String, List<String>> source) {
+      @SuppressWarnings("unchecked") // always safe
+      final CustomType<U, T> t = (CustomType<U, T>) TYPES.get(source.getKey());
+      return t != null ? Optional.of(new SimpleEntry<>(t, t.coercer.apply(t.supplyCoercer(source.getValue())))) : Optional.empty();
+    }
+
+    protected abstract U supplyCoercer(final List<String> parameters);
+  }
+
+  /**
+   * A {@link CustomType} that consumes all the matching query parameters as a
+   * {@code List<String>}.
+   *
+   * @param <T>
+   *          The coerced value for this {@code CustomType}
+   */
+  private static class AllParamsType<T> extends CustomType<List<String>, T> {
+
+    protected AllParamsType(final String name, final Function<? super List<String>, ? extends T> coercer, final @Nullable T orElse) {
+      super(name, coercer, orElse);
+    }
+
+    @Override
+    protected List<String> supplyCoercer(final List<String> parameters) {
+      return parameters;
+    }
+  }
+
+  /**
+   * A {@link CustomType} that only considers the first matching query parameter.
+   *
+   * @param <T>
+   *          The coerced value for this {@code CustomType}
+   */
+  private static class FirstParamType<T> extends CustomType<String, T> {
+
+    protected FirstParamType(final String name, final Function<String, ? extends T> coercer, final @Nullable T orElse) {
+      super(name, coercer, orElse);
+    }
+
+    @Override
+    protected String supplyCoercer(final List<String> parameters) {
+      return parameters.get(0);
     }
   }
 }
