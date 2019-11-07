f24aea5779ab127240f3e6bf8e0aaeed98ddaef1
ccjmne <ccjmne@gmail.com>
2019-11-07 03:39:18 +0100

    Properly handle ?group-by=* query parameter

--- src/main/java/org/ccjmne/orca/api/inject/business/QueryParams.java
+++ src/main/java/org/ccjmne/orca/api/inject/business/QueryParams.java
@@ -34,6 +34,8 @@ import com.fasterxml.jackson.databind.node.TextNode;
  */
 public class QueryParams {
 
+  private static final Param<JSONB> TAGS_FIELD_UNIVERSAL = DSL.val(JSONB.valueOf(new TextNode(Constants.TAGS_VALUE_UNIVERSAL).toString()));
+
   public static final FieldType<Integer> EMPLOYEE              = new FieldType<>("employee", Integer.class);
   public static final FieldType<Integer> SITE                  = new FieldType<>("site", Integer.class);
   public static final FieldType<Integer> SESSION               = new FieldType<>("session", Integer.class);
@@ -51,8 +53,8 @@ public class QueryParams {
                                                                                                   d -> Fields.DATE_INFINITY);
   public static final FirstParamType<Field<Date>>             INTERVAL      = new FirstParamType<>("interval", v -> DSL
       .field("{0}::interval", Date.class, v), DSL.field("'1 month'::interval", Date.class));
-  public static final FirstParamType<Field<JSONB>>            GROUP_BY      = new FirstParamType<>("group-by", v -> DSL
-      .field("site_tags -> {0}", JSONB.class, v), DSL.val(JSONB.valueOf(new TextNode(Constants.TAGS_VALUE_UNIVERSAL).toString())));
+  public static final FirstParamType<Field<JSONB>>            GROUP_BY      = new FirstParamType<>("group-by", v -> Constants.TAGS_VALUE_UNIVERSAL
+      .equals(v) ? TAGS_FIELD_UNIVERSAL : DSL.field("site_tags -> {0}", JSONB.class, v), TAGS_FIELD_UNIVERSAL);
 
   private static final Pattern IS_INFINITY_DATE = Pattern.compile("^-?infinity$");
   private static final Pattern IS_RELATIVE_DATE = Pattern.compile("^[+-]");
