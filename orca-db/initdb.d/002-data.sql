9e29b8b2e8e3604fe0049d499df0d80c3eee1b2a
Eric NICOLAS (ccjmne) <ccjmne@gmail.com>
2025-03-15 21:44:25 +0100

    Fix DB init script
    Hadn't been updated in *years*, apparently

--- orca-db/initdb.d/002-data.sql
+++ orca-db/initdb.d/002-data.sql
@@ -1,13 +1,11 @@
 INSERT INTO client (clnt_id, clnt_name, clnt_mailto, clnt_logo) VALUES
 	('demo', 'Version de démonstration', 'nclsdevelopment@gmail.com', 'https://s3-eu-west-1.amazonaws.com/orca-resources/logo_complete_small.png');
-INSERT INTO departments (dept_pk, dept_id, dept_name) VALUES
-	(0, 'NONE', 'Unassigned');
-INSERT INTO sites (site_pk, site_name, site_dept_fk) VALUES
-	(0, 'Unassigned', 0);
+INSERT INTO sites (site_pk, site_external_id, site_name) VALUES
+	(0, md5('Unassigned'), 'Unassigned');
 INSERT INTO employees (empl_pk, empl_firstname, empl_surname, empl_dob, empl_permanent, empl_gender) VALUES
-	('root', 'root', 'Orca', date '1990-05-15', false, false);
+	(0, 'root', 'Orca', date '1990-05-15', false, false);
 INSERT INTO users (user_id, user_pwd, user_type, user_empl_fk) VALUES
-	('root', md5('pwd'), 'employee', 'root');
+	('root', md5('pwd'), 'employee', 0);
 INSERT INTO trainerprofiles (trpr_pk, trpr_id) VALUES
 	(0, 'All');
 INSERT INTO users_roles (user_id, usro_type, usro_level, usro_trpr_fk) VALUES
@@ -15,3 +13,5 @@ INSERT INTO users_roles (user_id, usro_type, usro_level, usro_trpr_fk) VALUES
 	('root', 'access', 4, null),
 	('root', 'trainer', null, 0),
 	('root', 'admin', 4, null);
+INSERT INTO tags (tags_name, tags_type, tags_short) VALUES
+	('Département', 's', 'DEPT');
